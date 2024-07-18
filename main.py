dda7c53b3d131950a50d4ff4502bcdf79ffefad1
Wesley <machado.wesley1998@gmail.com>
2024-07-18 08:42:04 +0200

    We now send a GET_STAMPED.req with AC_CR to EID 7

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
+import os
+try:
+    os.environ['MK_PATH']
+except:
+    os.environ['MK_PATH'] = r"C:\Users\wesley.rodrigues\AXXES\OBU Proxy - Documents\16 - Automatisation des Tests\master_keys_test.json"
+
 import threading
 import logging
 import custom_der_decoders
+import key_derivation
 
 # Importing the definitions of the Python DLL loader, mainly consisting of enums and foreign functions
 # Function prototypes return foreign functions when called with a long pointer address, LPFN, as input
@@ -57,6 +64,27 @@ def main():
     root_logger.debug(f"Getting the attribute 32=0x20, PaymentMeans, for the instance with EID {eid}...")
     response = beacon_manager.get_request(eid, attribute_id_list=[0x20])
     root_logger.debug(f"BCM last command response in hex format: {beacon_manager.last_cmd_response.hex().upper()}")
+
+    eid = 7
+    # IssuerId = ContractProvider = EFC-CM
+    issuer_id = vst_data['applications'][1]['EFC-CM']
+
+    root_logger.debug(f"AC_CR-KeyRef in hex: {vst_data["AC_CR-KeyRef"]:04X}")
+    #ac_cr_mk_ref = vst_data["AC_CR-Ref"]["AC_CR-MasterKeyRef"]
+    #ac_cr_diversifier = vst_data["AC_CR-Ref"]["AC_CR-Diversifier"]
+    #root_logger.debug(f"AC_CR-MasterKeyReference in hex: {ac_cr_mack_ref:02X}")
+    #root_logger.debug(f"AC_CR-Diversifier in hex: {ac_cr_diversifier:02X}")
+    ac_cr_key_ref = vst_data["AC_CR-KeyRef"]
+
+    rnd_obe = vst_data["RndOBE"]
+    access_credentials = key_derivation.compute_access_credentials(issuer_id, rnd_obe, ac_cr_key_ref)
+    root_logger.debug(f"Generated Access Credentials in hex format: {access_credentials:08X}")
+
+    # Operator auth key is optional, we set it to 111 by default
+    #chosen_auth_key = 111
+    #beacon_manager.presentation_request(eid, access_credentials, chosen_auth_key)
+    beacon_manager.presentation_request(eid, access_credentials)
+
 
     root_logger.debug("We should send a SetMMI command on the main Thread to close the transaction")
     root_logger.debug("Otherwise, the transaction will remain unclosed and cause an error on the next execution")
