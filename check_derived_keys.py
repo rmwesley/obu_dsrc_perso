f00843bcd563e68e295e6ca760085e84283fde8d
Wesley <machado.wesley1998@gmail.com>
2024-09-04 18:19:09 +0200

    Added logging to derived key checking module

---
+++ check_derived_keys.py
@@ -0,0 +1,16 @@
+from key_derivation import decrypt_access_key, decrypt_auth_key
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+efc_cm = "C04001F200A6"
+print(f"Correct keys:")
+decrypt_access_key(efc_cm, bytes.fromhex("82E330789B8A6284"))
+decrypted_auk = decrypt_auth_key(efc_cm, bytes.fromhex("EBE97F200C853299"), 118)
+print(f"Decrypted AuK: {decrypted_auk.hex().upper()}\n\n")
+
+
+logging.error("WRONG Contract Provider used in key derivation, so keys are wrong:")
+print()
+decrypt_auth_key(efc_cm, bytes.fromhex("82287E9CA265A240"), 118)
+decrypt_auth_key(efc_cm, bytes.fromhex("6ED8F70749FD21B9"), 117)
+decrypt_auth_key(efc_cm, bytes.fromhex("82287E9CA265A240"), 111)
--- decrypt_derived_keys.py
+++
@@ -1,9 +0,0 @@
-from key_derivation import decrypt_access_key, decrypt_auth_key
-import logging
-logging.basicConfig(level=logging.DEBUG)
-
-efc_cm = "C04001F200A6"
-decrypt_access_key(efc_cm, bytes.fromhex("82E330789B8A6284"))
-decrypt_auth_key(efc_cm, bytes.fromhex("EBE97F200C853299"), 118)
-decrypt_auth_key(efc_cm, bytes.fromhex("82287E9CA265A240"), 118)
-decrypt_auth_key(efc_cm, bytes.fromhex("6ED8F70749FD21B9"), 117)
