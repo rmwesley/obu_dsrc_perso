d5569056470b07ef9f6d5c0aecce5d2070522781
Wesley <machado.wesley1998@gmail.com>
2024-10-22 14:25:56 +0200

    Beacon now uses EFC, CCC and LAC ASN specs

--- beacon_manager.py
+++ beacon_manager.py
@@ -1,6 +1,6 @@
-from ASN.compiled_DSRC_instances import CCCv4_1 as CCC2019
-from ASN.compiled_DSRC_instances import EFCv10_1 as EFC
-from ASN.compiled_DSRC_instances import LACv2_1
+# from ASN.compiled_DSRC_instances import CCCv4_1 as CCC2019
+# from ASN.compiled_DSRC_instances import EFCv10_1 as EFC
+from ASN.compiled_DSRC_instances import LACv2_1 as EFC_CCC_LAC_asn1_objs
 
 from datetime import datetime
 import json
@@ -42,14 +42,14 @@ class BeaconManager:
             'mandApplications': mandApplications,
             'profileList': profile_list
             }
-        EFC.EfcDsrcGeneric.BST.set_val(bst_value)
-        self.last_sent_bst = EFC.EfcDsrcGeneric.BST.to_uper()
+        EFC_CCC_LAC_asn1_objs.EfcDsrcGeneric.BST.set_val(bst_value)
+        self.last_sent_bst = EFC_CCC_LAC_asn1_objs.EfcDsrcGeneric.BST.to_uper()
         bcm_logger.debug(f"BST in UPER encoding in hex: {self.last_sent_bst.hex().upper()}")
 
-        EFC.EfcDsrcGeneric.T_APDUs.set_val(('initialisation-request', EFC.EfcDsrcGeneric.BST._val))
-        bcm_logger.debug(f"T_APDU containing BST in JER: {EFC.EfcDsrcGeneric.T_APDUs.to_jer()}")
+        EFC_CCC_LAC_asn1_objs.EfcDsrcGeneric.T_APDUs.set_val(('initialisation-request', EFC_CCC_LAC_asn1_objs.EfcDsrcGeneric.BST._val))
+        bcm_logger.debug(f"T_APDU containing BST in JER: {EFC_CCC_LAC_asn1_objs.EfcDsrcGeneric.T_APDUs.to_jer()}")
 
-        self.last_sent_t_apdu_containing_bst = EFC.EfcDsrcGeneric.T_APDUs.to_uper()
+        self.last_sent_t_apdu_containing_bst = EFC_CCC_LAC_asn1_objs.EfcDsrcGeneric.T_APDUs.to_uper()
 
         result = self.beacon_l7_wrapper.start_bst_wrapper(self.last_sent_t_apdu_containing_bst, bst_type)
 
@@ -89,16 +89,16 @@ class BeaconManager:
         
         bcm_logger.debug("We now remove the fragmentation header and instantiate an T_APDU object from the response!")
         t_apdu_init_resp_datagram = bytes(fragmented_t_apdu_init_resp_datagram[1:])
-        EFC.EfcDsrcGeneric.T_APDUs.from_uper(t_apdu_init_resp_datagram)
+        EFC_CCC_LAC_asn1_objs.EfcDsrcGeneric.T_APDUs.from_uper(t_apdu_init_resp_datagram)
         bcm_logger.debug(f"T-APDU without fragmentation header: {t_apdu_init_resp_datagram}")
         
         bcm_logger.debug("We now instantiate a T_APDU object from the response!")
-        bcm_logger.debug(f"Instantiated T_APDU object ASN1 decoding/representation: {EFC.EfcDsrcGeneric.T_APDUs.to_asn1()}")
-        bcm_logger.info(f"T_APDU containing VST in JER: {EFC.EfcDsrcGeneric.T_APDUs.to_jer()}")
-        bcm_logger.debug(f"Instantiated T_APDU object value: {EFC.EfcDsrcGeneric.T_APDUs._val}")
-        bcm_logger.info(f"Instantiated T_APDU in JER: {EFC.EfcDsrcGeneric.T_APDUs.to_jer()}")
-        self.last_response_t_apdu_json = EFC.EfcDsrcGeneric.T_APDUs._to_jval()
-        self.last_response_t_apdu_value = EFC.EfcDsrcGeneric.T_APDUs._val
+        bcm_logger.debug(f"Instantiated T_APDU object ASN1 decoding/representation: {EFC_CCC_LAC_asn1_objs.EfcDsrcGeneric.T_APDUs.to_asn1()}")
+        bcm_logger.info(f"T_APDU containing VST in JER: {EFC_CCC_LAC_asn1_objs.EfcDsrcGeneric.T_APDUs.to_jer()}")
+        bcm_logger.debug(f"Instantiated T_APDU object value: {EFC_CCC_LAC_asn1_objs.EfcDsrcGeneric.T_APDUs._val}")
+        bcm_logger.info(f"Instantiated T_APDU in JER: {EFC_CCC_LAC_asn1_objs.EfcDsrcGeneric.T_APDUs.to_jer()}")
+        self.last_response_t_apdu_json = EFC_CCC_LAC_asn1_objs.EfcDsrcGeneric.T_APDUs._to_jval()
+        self.last_response_t_apdu_value = EFC_CCC_LAC_asn1_objs.EfcDsrcGeneric.T_APDUs._val
 
         # Storing VST in field
         self.last_vst_json = self.last_response_t_apdu_json['initialisation-response']
@@ -115,23 +115,23 @@ class BeaconManager:
 
     def send_req_t_apdu_and_obtain_resp_t_apdu(self, asn1_request_t_apdu_value, close=False) -> dict:
         bcm_logger.debug(f"Preparing request T_APDU to be sent...")
-        EFC.EfcDsrcGeneric.T_APDUs.set_val(asn1_request_t_apdu_value)
-        bcm_logger.debug(f"Request T_APDU value: {EFC.EfcDsrcGeneric.T_APDUs._val}")
-        bcm_logger.debug(f"T_APDU in JER: {EFC.EfcDsrcGeneric.T_APDUs.to_jer()}")
+        EFC_CCC_LAC_asn1_objs.EfcDsrcGeneric.T_APDUs.set_val(asn1_request_t_apdu_value)
+        bcm_logger.debug(f"Request T_APDU value: {EFC_CCC_LAC_asn1_objs.EfcDsrcGeneric.T_APDUs._val}")
+        bcm_logger.debug(f"T_APDU in JER: {EFC_CCC_LAC_asn1_objs.EfcDsrcGeneric.T_APDUs.to_jer()}")
 
         # Sending command!!!
-        fragmented_t_apdu_with_get_response_bytes = self.beacon_l7_wrapper.send_command(EFC.EfcDsrcGeneric.T_APDUs.to_uper(), close)
+        fragmented_t_apdu_with_get_response_bytes = self.beacon_l7_wrapper.send_command(EFC_CCC_LAC_asn1_objs.EfcDsrcGeneric.T_APDUs.to_uper(), close)
 
         bcm_logger.debug(f"Decoding received response T_APDU...")
         bcm_logger.info(f"Fragmented T_APDU response obtained from beacon in hex (supposed to be UPER): {fragmented_t_apdu_with_get_response_bytes.hex().upper()}")
         t_apdu_with_response_bytes = bytes(fragmented_t_apdu_with_get_response_bytes[1:])
 
-        EFC.EfcDsrcGeneric.T_APDUs.from_uper(t_apdu_with_response_bytes)
+        EFC_CCC_LAC_asn1_objs.EfcDsrcGeneric.T_APDUs.from_uper(t_apdu_with_response_bytes)
 
-        self.last_response_t_apdu_value = EFC.EfcDsrcGeneric.T_APDUs._val
+        self.last_response_t_apdu_value = EFC_CCC_LAC_asn1_objs.EfcDsrcGeneric.T_APDUs._val
         bcm_logger.info(f"Response T-APDU value: {self.last_response_t_apdu_value}")
-        bcm_logger.info(f"Response T-APDU decoded with JER: {EFC.EfcDsrcGeneric.T_APDUs.to_jer()}")
-        self.last_response_t_apdu_json = EFC.EfcDsrcGeneric.T_APDUs._to_jval()
+        bcm_logger.info(f"Response T-APDU decoded with JER: {EFC_CCC_LAC_asn1_objs.EfcDsrcGeneric.T_APDUs.to_jer()}")
+        self.last_response_t_apdu_json = EFC_CCC_LAC_asn1_objs.EfcDsrcGeneric.T_APDUs._to_jval()
         bcm_logger.debug(f"Response T-APDU in JSON: {self.last_response_t_apdu_json}")
         
         bcm_logger.info(f"Checking if T-APDU contains a return (ret) value (error code)...")
@@ -187,7 +187,7 @@ class BeaconManager:
         access_credentials_bytes = access_credentials_int.to_bytes(4, 'big')
         return access_credentials_bytes
     
-    def send_get_request(self, eid, accessCredentialsPresent:bool = False, attrIdList=None, close_transaction = False) -> EFC.EfcDsrcGeneric.Get_Response:
+    def send_get_request(self, eid, accessCredentialsPresent:bool = False, attrIdList=None, close_transaction = False) -> EFC_CCC_LAC_asn1_objs.EfcDsrcGeneric.Get_Response:
         if accessCredentialsPresent:
             accessCredentials = self.compute_access_credentials(eid)
         else:
@@ -204,9 +204,9 @@ class BeaconManager:
         # This is specially the case for the OPTIONAL accessCredentials
         get_req_value = {key: value for key, value in get_req_value.items() if value is not None}
 
-        EFC.EfcDsrcGeneric.Get_Request.set_val(get_req_value)
-        bcm_logger.debug(f"Get.Request value: {EFC.EfcDsrcGeneric.Get_Request._val}")
-        bcm_logger.info(f"Get.Request in JER: {EFC.EfcDsrcGeneric.Get_Request.to_jer()}")
+        EFC_CCC_LAC_asn1_objs.EfcDsrcGeneric.Get_Request.set_val(get_req_value)
+        bcm_logger.debug(f"Get.Request value: {EFC_CCC_LAC_asn1_objs.EfcDsrcGeneric.Get_Request._val}")
+        bcm_logger.info(f"Get.Request in JER: {EFC_CCC_LAC_asn1_objs.EfcDsrcGeneric.Get_Request.to_jer()}")
 
         t_apdu_with_get_request_value = ('get-request', get_req_value)
         json_encoded_response_t_apdu = self.send_req_t_apdu_and_obtain_resp_t_apdu(t_apdu_with_get_request_value, close=close_transaction)
@@ -232,11 +232,11 @@ class BeaconManager:
         bcm_logger.info(f"ActionType value is '{actionParameter[0]}'")
 
         # ACTION.request has a parameter, which needs to be inside a container
-        EFC.EfcDsrcGeneric.EfcContainer.set_val(actionParameter)
-        parameter_tag = EFC.EfcDsrcGeneric.EfcContainer._tag
+        EFC_CCC_LAC_asn1_objs.EfcDsrcGeneric.EfcContainer.set_val(actionParameter)
+        parameter_tag = EFC_CCC_LAC_asn1_objs.EfcDsrcGeneric.EfcContainer._tag
 
-        bcm_logger.debug(f"ActionParameter is an EfcContainer of Type {actionParameter[0]} (tag {parameter_tag}) value decoded with JER: {EFC.EfcDsrcGeneric.EfcContainer.to_jer()}")
-        bcm_logger.debug(f"Same value decoded with APER in hex: {EFC.EfcDsrcGeneric.EfcContainer.to_aper().hex().upper()}")
+        bcm_logger.debug(f"ActionParameter is an EfcContainer of Type {actionParameter[0]} (tag {parameter_tag}) value decoded with JER: {EFC_CCC_LAC_asn1_objs.EfcDsrcGeneric.EfcContainer.to_jer()}")
+        bcm_logger.debug(f"Same value decoded with APER in hex: {EFC_CCC_LAC_asn1_objs.EfcDsrcGeneric.EfcContainer.to_aper().hex().upper()}")
         
         action_request_value = {
             'mode': mode,
@@ -256,8 +256,8 @@ class BeaconManager:
         t_apdu_with_action_req_value = ('action-request', action_request_value)
         bcm_logger.debug(f"T-APDU with ACTION.request value: {t_apdu_with_action_req_value}")
 
-        EFC.EfcDsrcGeneric.T_APDUs.set_val(t_apdu_with_action_req_value)
-        bcm_logger.info(f"T-APDU with ACTION.request in JER: {EFC.EfcDsrcGeneric.T_APDUs.to_jer()}")
+        EFC_CCC_LAC_asn1_objs.EfcDsrcGeneric.T_APDUs.set_val(t_apdu_with_action_req_value)
+        bcm_logger.info(f"T-APDU with ACTION.request in JER: {EFC_CCC_LAC_asn1_objs.EfcDsrcGeneric.T_APDUs.to_jer()}")
         bcm_logger.info(f"ACTION.request with ActionType {actionType} and actionParameter of type {actionParameter[0]} being now sent...")
 
         json_encoded_response_t_apdu = self.send_req_t_apdu_and_obtain_resp_t_apdu(t_apdu_with_action_req_value, close)
@@ -323,10 +323,10 @@ class BeaconManager:
         bcm_logger.info(f'attributeList value: {attributeList}')
 
         container_with_attribute_list = ('attrList', get_stamped_rs['attributeList'])
-        EFC.EfcDsrcGeneric.EfcContainer.set_val(container_with_attribute_list)
-        bcm_logger.info(f"EFC Container of Type/CHOICE 'attrList' value: {EFC.EfcDsrcGeneric.EfcContainer._val}")
+        EFC_CCC_LAC_asn1_objs.EfcDsrcGeneric.EfcContainer.set_val(container_with_attribute_list)
+        bcm_logger.info(f"EFC Container of Type/CHOICE 'attrList' value: {EFC_CCC_LAC_asn1_objs.EfcDsrcGeneric.EfcContainer._val}")
 
-        attribute_list_bytes = EFC.EfcDsrcGeneric.EfcContainer.to_uper()[1:]
+        attribute_list_bytes = EFC_CCC_LAC_asn1_objs.EfcDsrcGeneric.EfcContainer.to_uper()[1:]
 
         provided_authenticator = get_stamped_rs['authenticator']
         rnd_rse_bytes = self.rnd_rse_bytes_value
@@ -349,7 +349,7 @@ class BeaconManager:
     def update_rnd_rse(self):
         bcm_logger.debug(f"Updating DateAndTime/SessionTime value (to be used as RndRSE value)...")
 
-        EFC.EfcDataDictionary.DateAndTime.set_val({ 
+        EFC_CCC_LAC_asn1_objs.EfcDataDictionary.DateAndTime.set_val({ 
             'timeDate':{
                 'year': datetime.utcnow().year,
                 'month': datetime.utcnow().month,
@@ -362,8 +362,8 @@ class BeaconManager:
             }
         })
 
-        bcm_logger.debug(f"RndRSE or SessionTime value (of type DateAndTime) in JER: {EFC.EfcDataDictionary.DateAndTime.to_jer()}")
-        self.rnd_rse_bytes_value = EFC.EfcDataDictionary.DateAndTime.to_uper()
+        bcm_logger.debug(f"RndRSE or SessionTime value (of type DateAndTime) in JER: {EFC_CCC_LAC_asn1_objs.EfcDataDictionary.DateAndTime.to_jer()}")
+        self.rnd_rse_bytes_value = EFC_CCC_LAC_asn1_objs.EfcDataDictionary.DateAndTime.to_uper()
 
         bcm_logger.debug(f"RndRSE value in UPER in hex: {self.rnd_rse_bytes_value.hex().upper()}")
         return self.rnd_rse_bytes_value
@@ -389,10 +389,10 @@ class BeaconManager:
             'keyRef': operator_auk_ref
             }
         bcm_logger.debug(f"GetStampedRq value to be stored in definition: {get_stamped_rq_value}")
-        EFC.EfcDsrcApplication.GetStampedRq.set_val(get_stamped_rq_value)
+        EFC_CCC_LAC_asn1_objs.EfcDsrcApplication.GetStampedRq.set_val(get_stamped_rq_value)
         
-        bcm_logger.debug(f"GetStampedRq value: {EFC.EfcDsrcApplication.GetStampedRq._val}")
-        bcm_logger.info(f"GetStampedRs in JER: {EFC.EfcDsrcApplication.GetStampedRq.to_jer()}")
+        bcm_logger.debug(f"GetStampedRq value: {EFC_CCC_LAC_asn1_objs.EfcDsrcApplication.GetStampedRq._val}")
+        bcm_logger.info(f"GetStampedRs in JER: {EFC_CCC_LAC_asn1_objs.EfcDsrcApplication.GetStampedRq.to_jer()}")
         return get_stamped_rq_value
 
     def set_mmi(self, eid=0, close=False):
@@ -402,9 +402,9 @@ class BeaconManager:
         set_mmi_request_value = 0
         # SetMMI is a parameterized type, so it needs to be inside a container
         set_mmi_efc_container_value = ('setmmirq', set_mmi_request_value)
-        EFC.EfcDsrcGeneric.EfcContainer.set_val(set_mmi_efc_container_value)
-        bcm_logger.debug(f"EfcContainer of Type 69 (SET_MMI) value decoded with JER: {EFC.EfcDsrcGeneric.EfcContainer.to_jer()}")
-        bcm_logger.debug(f"EfcContainer of Type 69 (SET_MMI) value decoded with PER: {EFC.EfcDsrcGeneric.EfcContainer.to_uper()}")
+        EFC_CCC_LAC_asn1_objs.EfcDsrcGeneric.EfcContainer.set_val(set_mmi_efc_container_value)
+        bcm_logger.debug(f"EfcContainer of Type 69 (SET_MMI) value decoded with JER: {EFC_CCC_LAC_asn1_objs.EfcDsrcGeneric.EfcContainer.to_jer()}")
+        bcm_logger.debug(f"EfcContainer of Type 69 (SET_MMI) value decoded with PER: {EFC_CCC_LAC_asn1_objs.EfcDsrcGeneric.EfcContainer.to_uper()}")
         
         # SetMMI ActionType is 0xA, or 10 in decimal
         set_mmi_action_request_val = {
