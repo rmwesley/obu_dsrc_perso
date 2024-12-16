632b622da2503cbe16663bae898abc08e091fef2
Wesley <machado.wesley1998@gmail.com>
2024-12-16 18:00:21 +0100

    Compiled a custom ASN spec with an 8-bit CV type
    CV should be at most 7 bits according to ISO 17573, EfcDataDictionary,
    but somme Toll Chargers accept 8 bits.

--- compile_asn_specs.ps1
+++ compile_asn_specs.ps1
@@ -1,7 +1,8 @@
 # .\.venv\Scripts\python.exe .\.venv\Scripts\pycrate_asn1compile.py -i "./ASN/DSRC Instances asn specs/EFC v10.1/" -o ./ASN/compiled_DSRC_instances/EFCv10_1
-.\.venv\Scripts\python.exe .\.venv\Scripts\pycrate_asn1compile.py -i "./ASN/DSRC Instances asn specs/CCC v4.1/" -o ./ASN/compiled_DSRC_instances/CCCv4_1
+# .\.venv\Scripts\python.exe .\.venv\Scripts\pycrate_asn1compile.py -i "./ASN/DSRC Instances asn specs/CCC v4.1/" -o ./ASN/compiled_DSRC_instances/CCCv4_1
 # .\.venv\Scripts\python.exe .\.venv\Scripts\pycrate_asn1compile.py -i "./ASN/DSRC Instances asn specs/CCC v1/" -o ./ASN/compiled_DSRC_instances/CCCv1
-.\.venv\Scripts\python.exe .\.venv\Scripts\pycrate_asn1compile.py -i "./ASN/DSRC Instances asn specs/LAC v2.1/" -o ./ASN/compiled_DSRC_instances/LACv2_1
+# .\.venv\Scripts\python.exe .\.venv\Scripts\pycrate_asn1compile.py -i "./ASN/DSRC Instances asn specs/LAC v2.1/" -o ./ASN/compiled_DSRC_instances/LACv2_1
+.\.venv\Scripts\python.exe .\.venv\Scripts\pycrate_asn1compile.py -i "./ASN/DSRC Instances asn specs/EFC CCC 2023 with 8 bits CV/" -o ./ASN/compiled_DSRC_instances/modified_CCC_2023
 
 # .\.venv\Scripts\python.exe .\.venv\Scripts\pycrate_asn1compile.py -i "./ASN/infoExchange ISO 12855/infoExchangev5/" -o ./ASN/compiled_tsp_tc_info_exchange/EfcInfoExchangev5
 # .\.venv\Scripts\python.exe .\.venv\Scripts\pycrate_asn1compile.py -i "./ASN/infoExchange ISO 12855/infoExchangev4 with X509 2012 and RFC5035/" -o ./ASN/compiled_tsp_tc_info_exchange/EfcInfoExchangev4
