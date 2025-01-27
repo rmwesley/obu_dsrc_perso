0d8aa7e425bde9f6079bc074e53c0a0d6af0acfd
Wesley <machado.wesley1998@gmail.com>
2025-01-27 12:41:52 +0100

    Basic ASN1 defs for OPS1955 beacon Message Types

--- compile_asn_specs.ps1
+++ compile_asn_specs.ps1
@@ -4,7 +4,9 @@
 # .\.venv\Scripts\python.exe .\.venv\Scripts\pycrate_asn1compile.py -i "./ASN/DSRC Instances asn specs/LAC v2.1/" -o ./ASN/compiled_DSRC_instances/LACv2_1
 
 # .\.venv\Scripts\python.exe .\.venv\Scripts\pycrate_asn1compile.py -i "./ASN/DSRC Instances asn specs/EFC CCC 2023 with 8 bits CV/" -o ./ASN/compiled_DSRC_instances/modified_CCC_2023
-.\.venv\Scripts\python.exe .\.venv\Scripts\pycrate_asn1compile.py -i "./ASN/DSRC Instances asn specs/EFC DSRC 2023/" -o ./ASN/compiled_DSRC_instances/EN15509
+.\.venv\Scripts\python.exe .\.venv\Scripts\pycrate_asn1compile.py -i "./ASN/DSRC Instances asn specs/EFC DSRC 2023/" -o ./ASN/compiled_DSRC_instances/DSRCv10
+.\.venv\Scripts\python.exe .\.venv\Scripts\pycrate_asn1compile.py -i "./ASN/DSRC Instances asn specs/EFC EU Wesley 2025/" -o ./ASN/compiled_DSRC_instances/EN15509
+.\.venv\Scripts\python.exe .\.venv\Scripts\pycrate_asn1compile.py -i "./ASN/Custom Kapsch OPS1955 beacon ASN specs/" -o ./ASN/compiled_DSRC_instances/OPS1955
 
 # .\.venv\Scripts\python.exe .\.venv\Scripts\pycrate_asn1compile.py -i "./ASN/infoExchange ISO 12855/infoExchangev5/" -o ./ASN/compiled_tsp_tc_info_exchange/EfcInfoExchangev5
 # .\.venv\Scripts\python.exe .\.venv\Scripts\pycrate_asn1compile.py -i "./ASN/infoExchange ISO 12855/infoExchangev4 with X509 2012 and RFC5035/" -o ./ASN/compiled_tsp_tc_info_exchange/EfcInfoExchangev4
