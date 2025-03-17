7399ad1cb702e5d2baff92661ee654b72d1ac1c8
Wesley <machado.wesley1998@gmail.com>
2025-03-17 20:43:25 +0100

    Added TGB L7 commands stub methods

--- bac_l7/tgbv_bac_interface.py
+++ bac_l7/tgbv_bac_interface.py
@@ -1,4 +1,13 @@
 from bac_l7 import pertel_bac_interface
 
 class TgbvBacL2(pertel_bac_interface.PertelBacL2):
-    pass
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+    def _tgb_change_beacon_id(self) -> bytes:
+        pass
+    def _tgb_read_beacon_id(self) -> bytes:
+        pass
+    def _tgb_change_communication_config(self) -> bytes:
+        pass
+    def _tgb_read_communication_config(self) -> bytes:
+        pass
