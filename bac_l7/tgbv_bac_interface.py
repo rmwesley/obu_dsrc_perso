4e7ab207d38a2a74abe16a6024f966eca2036df6
Wesley <machado.wesley1998@gmail.com>
2025-03-18 10:11:00 +0100

    Added method to get BeaconID from TGB in 6 bytes!!

--- bac_l7/tgbv_bac_interface.py
+++ bac_l7/tgbv_bac_interface.py
@@ -3,11 +3,25 @@ from bac_l7 import pertel_bac_interface
 class TgbvBacL2(pertel_bac_interface.PertelBacL2):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
     def _tgb_change_beacon_id(self) -> bytes:
+        """Command ID 0x53"""
         pass
+
     def _tgb_read_beacon_id(self) -> bytes:
-        pass
+        """Command ID 0x54"""
+        message_content = bytes([0x54])
+        return self.send_command(message_content)
+
+    def get_beacon_id(self) -> bytes:
+        response_content = self._tgb_read_beacon_id()
+        beacon_id = bytes(response_content[3:9])
+        return beacon_id
+
     def _tgb_change_communication_config(self) -> bytes:
+        """Command ID 0x55"""
         pass
+
     def _tgb_read_communication_config(self) -> bytes:
+        """Command ID 0x56"""
         pass
