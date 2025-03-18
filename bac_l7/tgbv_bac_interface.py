72b7fc576e566081ace702628595bd46101e48d2
Wesley <machado.wesley1998@gmail.com>
2025-03-18 11:32:06 +0100

    Added method to get latest BeaconID from BAC L7

--- bac_l7/tgbv_bac_interface.py
+++ bac_l7/tgbv_bac_interface.py
@@ -3,6 +3,7 @@ from bac_l7 import pertel_bac_interface
 class TgbvBacL2(pertel_bac_interface.PertelBacL2):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.beacon_id = bytes(6)
 
     def _tgb_change_beacon_id(self) -> bytes:
         """Command ID 0x53"""
@@ -13,10 +14,13 @@ class TgbvBacL2(pertel_bac_interface.PertelBacL2):
         message_content = bytes([0x54])
         return self.send_command(message_content)
 
-    def get_beacon_id(self) -> bytes:
+    def update_beacon_id(self) -> bytes:
         response_content = self._tgb_read_beacon_id()
-        beacon_id = bytes(response_content[3:9])
-        return beacon_id
+        self.beacon_id = bytes(response_content[3:9])
+        return self.beacon_id
+
+    def get_beacon_id(self) -> bytes:
+        return self.beacon_id
 
     def _tgb_change_communication_config(self) -> bytes:
         """Command ID 0x55"""
