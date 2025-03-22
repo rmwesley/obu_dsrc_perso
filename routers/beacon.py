48ec3b1fe6ae8f4c01afb4007e11fed3afd1d71e
Wesley <machado.wesley1998@gmail.com>
2025-03-22 02:46:38 +0100

    Imported new RSE DSRC L7 module

--- routers/beacon.py
+++ routers/beacon.py
@@ -8,6 +8,7 @@ from typing import Literal, Optional
 from enum import IntEnum
 
 import dsrc_security
+from dsrc_l7 import dsrc_l7_rse
 
 router = APIRouter(
     prefix="/beacon",
@@ -30,14 +31,14 @@ async def favicon():
 async def initialize_beacon_manager():
     ''' Initialize the BeaconManager
     '''
-    beacon_manager_module.initialize_bcm()
+    dsrc_l7_rse.initialize_bcm()
     return "Beacon Manager was intialized!"
 
 @router.post("/reset-beacon")
 async def shutdown():
     ''' Reset beacon
     '''
-    beacon_manager_module.reset_beacon()
+    dsrc_l7_rse.reset_beacon()
     return "Beacon Manager was shut down!"
 
 @router.post("/shutdown-manager")
@@ -46,7 +47,7 @@ async def shutdown():
         Close the connection
         Clear variables and release the resources
     '''
-    beacon_manager_module.shutdown_beacon()
+    dsrc_l7_rse.shutdown_beacon()
     return "Beacon Manager was shut down!"
 
 class LoopRequest(BaseModel):
@@ -56,22 +57,22 @@ async def loop_transactions(loop_req: LoopRequest):
     ''' Manage Loop Transactions
     '''
     if loop_req.loop_state == 'ON':
-        beacon_manager_module.loop_transactions()
+        dsrc_l7_rse.loop_transactions()
         return "looping transactions!!"
     if loop_req.loop_state == 'OFF':
-        beacon_manager_module.stop_loop()
+        dsrc_l7_rse.stop_loop()
         return "Killed loop!!"
 
 # Endpoint to get the current beacon state
 @router.get("/beacon-state")
 async def get_beacon_state():
-    return {"beacon_state": beacon_manager_module.get_last_beacon_state()}
+    return {"beacon_state": dsrc_l7_rse.get_last_beacon_state()}
 
 # Endpoint to update the current beacon state
 @router.post("/update-beacon-state")
 async def update_beacon_state():
-    beacon_manager_module.update_state()
-    return {"beacon_state": beacon_manager_module.get_last_beacon_state()}
+    dsrc_l7_rse.update_state()
+    return {"beacon_state": dsrc_l7_rse.get_last_beacon_state()}
 
 
 class ChangeModeRequest(BaseModel):
@@ -96,7 +97,7 @@ class ChangeModeRequest(BaseModel):
 @router.post("/change-mode")
 async def change_mode(request_body: ChangeModeRequest):
     mode_name = request_body.mode_name
-    beacon_manager_module.change_trx_mode(mode_name=mode_name)
+    dsrc_l7_rse.change_trx_mode(mode_name=mode_name)
     return {"message": f"Changed mode to {mode_name}!"}
 
 @router.post("/initialize-transaction")
@@ -104,14 +105,14 @@ async def initialize_transaction():
     """Endpoint to initialize a transaction (send a BST and get a VST)
     After intializing a transactions, the EFC functions can be called"""
     try:
-        last_decoded_vst_obj = beacon_manager_module.initialize_transaction()
+        last_decoded_vst_obj = dsrc_l7_rse.initialize_transaction()
     except BeaconManagerException as beacon_error:
         raise HTTPException(status_code=400, detail=f"{type(beacon_error).__name__}: {beacon_error}")
     return {"last_vst": last_decoded_vst_obj}
 
 @router.get("/last-transaction-init-data")
 async def get_last_initialization_data():
-    return beacon_manager_module.get_init_data()
+    return dsrc_l7_rse.get_init_data()
 
 class GetRequest(BaseModel):
     eid:int
@@ -133,7 +134,7 @@ class GetRequest(BaseModel):
     }
 @router.post("/send-get-request")
 async def send_get_request(request_body: GetRequest):
-    beacon_manager_module.send_get_request(**request_body.dict())
+    dsrc_l7_rse.send_get_request(**request_body.dict())
     return "Success!"
 
 class PresentationReq(BaseModel):
@@ -158,35 +159,35 @@ class PresentationReq(BaseModel):
     }
 @router.post("/send-presentation-request")
 async def send_presentation_request(request_body: PresentationReq):
-    get_stamped_response_json = beacon_manager_module.presentation_request(**request_body.dict())
+    get_stamped_response_json = dsrc_l7_rse.presentation_request(**request_body.dict())
     return "Success!"
 
 # Endpoints to close transaction
 @router.post("/send-close-transaction-echo-to-obu")
 async def send_close_transaction_echo_to_obu():
-    beacon_manager_module.send_close_transaction_echo()
+    dsrc_l7_rse.send_close_transaction_echo()
     return "Success!"
 
 @router.post("/send-close-transaction-set-mmi-to-obu")
 async def send_close_set_mmi_to_obu():
-    beacon_manager_module.send_close_transaction_setmmi()
+    dsrc_l7_rse.send_close_transaction_setmmi()
     return "Success!"
 
 # Endpoint to initialize the beacon and close transaction
 @router.post("/initialize-and-close-transaction")
 async def initialize_and_close_transaction():
-    beacon_manager_module.initialize_transaction()
+    dsrc_l7_rse.initialize_transaction()
 
-    beacon_manager_module.send_close_transaction_setmmi()
+    dsrc_l7_rse.send_close_transaction_setmmi()
     return "Success!"
 
 @router.get("/last-response-t-apdu-with-vst-json")
 async def get_last_vst():
-    return beacon_manager_module.last_response_t_apdu_with_vst_json
+    return dsrc_l7_rse.last_response_t_apdu_with_vst_json
 
 @router.get("/last-response-t-apdu-json")
 async def get_last_vst():
-    return beacon_manager_module.last_response_t_apdu_json
+    return dsrc_l7_rse.last_response_t_apdu_json
 
 class EFCFunctionRequest(BaseModel):
     function_type: str
@@ -199,8 +200,8 @@ class TransactionReq(BaseModel):
 
 @router.post("/CARDME")
 async def cardme(request_body: TransactionReq):
-    return beacon_manager_module.cardme_transaction(request_body.eid)
+    return dsrc_l7_rse.cardme_transaction(request_body.eid)
 
 @router.post("/Get-all-128-attrs")
 async def get_all_attributes(request_body: TransactionReq):
-    return beacon_manager_module.get_all_attributes(request_body.eid)
+    return dsrc_l7_rse.get_all_attributes(request_body.eid)
