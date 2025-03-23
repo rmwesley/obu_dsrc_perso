7cae5c0ca7f6484df9fe82b4ed8d0acb32aa7368
Wesley <machado.wesley1998@gmail.com>
2025-03-23 10:47:40 +0100

    Added async handling of BAC L7 messages

--- bac_l7/ops1955_bac_l7.py
+++ bac_l7/ops1955_bac_l7.py
@@ -10,17 +10,17 @@ class Ops1955BacL7(pertel_bac_l7.PertelBacL7):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-    def _kapsch_set_config(self):
+    async def _kapsch_set_config(self):
         # STOP the beacon first!!
-        self._pertel_set_beacon_mode(0x03)
-        self._pertel_monitor_beacon()
+        await self._pertel_set_beacon_mode(0x03)
+        await self._pertel_monitor_beacon()
 
-        self._kapsch_cd_read_dsrc_config()
-        self._kapsch_cd_set_dsrc_config()
+        await self._kapsch_cd_read_dsrc_config()
+        await self._kapsch_cd_set_dsrc_config()
         # We now set its mode to transparent and stop serial communication!
-        self._pertel_set_beacon_mode(0x01)
+        await self._pertel_set_beacon_mode(0x01)
 
-    def _kapsch_cd_set_dsrc_config(self) -> bytes:
+    async def _kapsch_cd_set_dsrc_config(self) -> bytes:
         """
         Kapsch-specific.
         CD_SET_DSRC_CONFIG
@@ -38,9 +38,9 @@ class Ops1955BacL7(pertel_bac_l7.PertelBacL7):
             end_transceiver_behavior = 0x01
 
         message_content = bytes([msg_id, msg_data_struct_version, dsrc_channel, bst_repetition_time, end_transceiver_behavior])
-        return self.send_command(message_content=message_content)
+        return await self.send_command(message_content=message_content)
 
-    def _kapsch_cd_read_dsrc_config(self) -> bytes:
+    async def _kapsch_cd_read_dsrc_config(self) -> bytes:
         """
         Kapsch-specific.
         CD_READ_DSRC_CONFIG
@@ -48,4 +48,4 @@ class Ops1955BacL7(pertel_bac_l7.PertelBacL7):
         """
         msg_id = 0x62 # 98
         message_content = bytes([msg_id])
-        return self.send_command(message_content=message_content)
+        return await self.send_command(message_content=message_content)
--- bac_l7/pertel_bac_l7.py
+++ bac_l7/pertel_bac_l7.py
@@ -72,7 +72,7 @@ class PertelBacL7(bac_l2_host2beacon.BacHost):
         # return response_content, decoded_response
         return response_content
 
-    def _pertel_monitor_beacon(self) -> bytes:
+    async def _pertel_monitor_beacon(self) -> bytes:
         """
         Request:
             Command ID, 1 byte:
@@ -95,7 +95,7 @@ class PertelBacL7(bac_l2_host2beacon.BacHost):
                 0x01: True
         """
         message_content = bytes([0x01])
-        return self.send_command(message_content)
+        return await self.send_command(message_content)
     def update_state(self) -> bytes:
         response_content = self._pertel_monitor_beacon()
         self.beacon_state = bytes(response_content[1:])
@@ -112,7 +112,13 @@ class PertelBacL7(bac_l2_host2beacon.BacHost):
     def _is_transaction_in_progress(self) -> bool:
         return self.t_apdu_containing_vst != None
 
-    def _pertel_emit_bst_and_get_vst(self, t_apdu_containing_bst:bytes) -> bytes:
+    # def pertel_start_bst_emission_and_get_vst(self, t_apdu_containing_bst: bytes, change_beacon_id_periodically:bool = True) -> bytes:
+    #     """Emits BST using helper methods, based on the choice of BeaconID behavior (Normal or Periodically changed)"""
+    #     if not change_beacon_id_periodically:
+    #         return self._pertel_emit_bst_and_get_vst_without_changing_beacon_id(t_apdu_containing_bst)
+    #     return self._pertel_emit_bst_and_get_vst(t_apdu_containing_bst)
+        
+    async def _pertel_start_bst_emission_and_await_vst(self, t_apdu_containing_bst:bytes) -> bytes:
         """
         Least significant 3 bits (modulo 8) of BeaconID (individualId) are incremented:
             Every 128 emissions, or
@@ -128,7 +134,7 @@ class PertelBacL7(bac_l2_host2beacon.BacHost):
             VST, variable size:
         """
         message_content = bytes([0x03]) + t_apdu_containing_bst
-        response_content = self.send_command(message_content)
+        response_content = await self.send_command(message_content)
 
         if response_content[1] == 0:
             # Removing Command ID 0x03
@@ -136,12 +142,6 @@ class PertelBacL7(bac_l2_host2beacon.BacHost):
             return response_content
         else:
             raise PertelBacL7Exception(f'Error response when requesting for BST!! Response message: 0x{response_content.hex().upper()}')
-
-    def pertel_start_bst_emission_and_get_vst(self, t_apdu_containing_bst: bytes, change_beacon_id_periodically:bool = True) -> bytes:
-        """Emits BST using helper methods, based on the choice of BeaconID behavior (Normal or Periodically changed)"""
-        if not change_beacon_id_periodically:
-            return self._pertel_emit_bst_and_get_vst_without_changing_beacon_id(t_apdu_containing_bst)
-        return self._pertel_emit_bst_and_get_vst(t_apdu_containing_bst)
 
     def _pertel_stop_bst_emission(self, t_apdu_bst_datagram:bytes) -> bytes:
         """
@@ -160,7 +160,13 @@ class PertelBacL7(bac_l2_host2beacon.BacHost):
         message_content = bytes([0x04])
         return self.send_command(message_content)
 
-    def _pertel_send_dsrc_l7_command_and_close_transaction(self, t_apdu_containing_request:bytes) -> bytes:
+    def _pertel_send_dsrc_l7_command_with_close_transaction_option(self, t_apdu_containing_request:bytes, close_transaction:bool) -> bytes:
+        if close_transaction:
+            self._pertel_send_dsrc_l7_command(t_apdu_containing_request)
+        else:
+            self._pertel_send_dsrc_l7_command_and_close_transaction(t_apdu_containing_request)
+
+    async def _pertel_send_dsrc_l7_command(self, t_apdu_containing_request:bytes) -> bytes:
         """
         Request:
             Command ID, 1 byte:
@@ -181,7 +187,7 @@ class PertelBacL7(bac_l2_host2beacon.BacHost):
             Response_T_APDU, variable:
         """
         message_content = bytes([0x05]) + t_apdu_containing_request
-        response_content = self.send_command(message_content)
+        response_content = await self.send_command(message_content)
 
         # Removing Command ID 0x05
         self.t_apdu_containing_response = response_content[1:]
@@ -191,7 +197,7 @@ class PertelBacL7(bac_l2_host2beacon.BacHost):
 
         return response_content
 
-    def _pertel_send_dsrc_l7_command_and_close_transaction(self, t_apdu_containing_request:bytes) -> bytes:
+    async def _pertel_send_dsrc_l7_command_and_close_transaction(self, t_apdu_containing_request:bytes) -> bytes:
         """
         Request:
             Command ID, 1 byte:
@@ -212,12 +218,12 @@ class PertelBacL7(bac_l2_host2beacon.BacHost):
             Response_T_APDU, variable:
         """
         message_content = bytes([0x06]) + t_apdu_containing_request
-        response_content = self.send_command(message_content)
+        response_content = await self.send_command(message_content)
         # Removing Command ID 0x06
         self.t_apdu_containing_response = response_content[1:]
         return response_content
 
-    def _pertel_change_config(self, config_parameters:bytes) -> bytes:
+    async def _pertel_change_config(self, config_parameters:bytes) -> bytes:
         """
         Request:
             Command ID, 1 byte:
@@ -230,11 +236,11 @@ class PertelBacL7(bac_l2_host2beacon.BacHost):
             VST, variable size:
             """
         message_content = bytes([0x15]) + config_parameters
-        response_content = self.send_command(message_content)
+        response_content = await self.send_command(message_content)
 
         return response_content
 
-    def _pertel_read_version_and_config(self, t_apdu_containing_bst:bytes) -> bytes:
+    async def _pertel_read_version_and_config(self, t_apdu_containing_bst:bytes) -> bytes:
         """
         Request:
             Command ID, 1 byte:
@@ -247,11 +253,11 @@ class PertelBacL7(bac_l2_host2beacon.BacHost):
             VST, variable size:
             """
         message_content = bytes([0x16]) + t_apdu_containing_bst
-        response_content = self.send_command(message_content)
+        response_content = await self.send_command(message_content)
 
         return response_content
 
-    def _pertel_emit_bst_and_get_vst_without_changing_beacon_id(self, t_apdu_containing_bst:bytes) -> bytes:
+    async def _pertel_emit_bst_and_get_vst_without_changing_beacon_id(self, t_apdu_containing_bst:bytes) -> bytes:
         """
         Request:
             Command ID, 1 byte:
@@ -264,13 +270,13 @@ class PertelBacL7(bac_l2_host2beacon.BacHost):
             VST, variable size:
             """
         message_content = bytes([0x17]) + t_apdu_containing_bst
-        response_content = self.send_command(message_content)
+        response_content = await self.send_command(message_content)
 
         # Removing Command ID 0x17
         self.t_apdu_containing_vst = response_content[1:]
         return response_content
 
-    def _pertel_reset_beacon(self) -> bytes:
+    async def _pertel_reset_beacon(self) -> bytes:
         """
         This command resets the communication and error counters!
         Request:
@@ -283,4 +289,4 @@ class PertelBacL7(bac_l2_host2beacon.BacHost):
             Error, 1 byte:
             """
         message_content = bytes([0x1E])
-        return self.send_command(message_content)
+        return await self.send_command(message_content)
