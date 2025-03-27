1e274e0834bac91ae168326968ad613aeab8812b
Wesley <machado.wesley1998@gmail.com>
2025-03-27 01:05:02 +0100

    Added custom Baudot decoder to ITS decoders
    Also simplified example code snippet

--- custom_its_per_decoders.py
+++ custom_its_per_decoders.py
@@ -1,7 +1,60 @@
 # from ASN.compiled_DSRC_instances import LACv2_1 as EFC_CCC_LAC_asn1_objs
 from ASN.compiled_DSRC_instances import AXXESv1_1 as EFC_CCC_LAC_asn1_objs
-
+import baudot
 import logging
+import io
+
+def split_country_code_baudot_chars_in_bytes(country_code:int) -> bytes:
+    first_5bits = (country_code >> 5) & 0b11111
+    second_5bits = country_code & 0b11111
+    return bytes([first_5bits, second_5bits])
+
+def reverse_mask_5_bits(x:int) -> int:
+    x = ((x & 0x05) << 1) | ((x & 0x0A) >> 1) | (x & 0x10)
+    x = ((x & 0x03) << 2) | ((x & 0x0C) >> 2) | (x & 0x10)
+    x = ((x & 0x0F) << 1) | ((x & 0x10) >> 4)
+    return x
+
+class ReadError(Exception):
+     pass
+class BaudotMsbFirstBytesReader(baudot.handlers.core.BaudotReader):
+    def __init__(self, stream: io.BufferedIOBase):
+        self.stream = stream
+
+    def __next__(self):
+        next_byte = self.stream.read(1)
+        if not next_byte:
+            raise StopIteration()
+        try:
+            code = next_byte[0]
+            code = reverse_mask_5_bits(code)
+        except ValueError:
+            str_repr = next_byte.decode(errors='backslashreplace')
+            raise ReadError(f'Invalid hexadecimal byte: {str_repr}')
+        if not 0 <= code < 32:
+            raise ReadError(f'Code value {code} is not a valid 5-bit value')
+        # print(code)
+        return code
+
+def decode_country_code(country_code:str) -> str:
+    if type(country_code) is int:
+        return decode_country_code_from_int(country_code)
+    return decode_country_code_from_hex_str(country_code)
+
+def decode_country_code_from_int(country_code_with_lsb_to_right_int:int) -> str:
+    baudot_stream = split_country_code_baudot_chars_in_bytes(country_code_with_lsb_to_right_int)
+    
+    # ITA2_SWITCH_CODE: 0x1F
+    code = bytes([0x1F]) + baudot_stream
+    with io.BytesIO(code) as country_code_bitstream:
+        reader = BaudotMsbFirstBytesReader(country_code_bitstream)
+        alpha2 = baudot.decode_to_str(reader, baudot.codecs.ITA2_STANDARD)
+    return alpha2
+
+def decode_country_code_from_hex_str(country_code_with_msb_to_left_hex_str:str) -> str:
+    country_code_12_bits = country_code_with_msb_to_left_hex_str[0:3]
+    country_code_with_lsb_to_right_int = int(country_code_12_bits, 16) >> 2
+    return decode_country_code_from_int(country_code_with_lsb_to_right_int)
 
 custom_per_decoders_logger = logging.getLogger()
 def decode_vst_parameter_oct_str_bytes(parameter_bytes):
--- snippets/baudot/baudot_test.py
+++ snippets/baudot/baudot_test.py
@@ -2,46 +2,10 @@
 import baudot
 from io import BytesIO, BufferedIOBase
 
-import baudot.handlers
+from custom_its_per_decoders import decode_country_code_from_int, decode_country_code_from_hex_str
 
 country_code = 0xb280 >> 6
 
-def split_country_code_baudot_chars_in_bytes(country_code:int) -> bytes:
-    first_5bits = (country_code >> 5) & 0b11111
-    second_5bits = country_code & 0b11111
-    return bytes([first_5bits, second_5bits])
-
-def reverse_mask_5_bits(x:int) -> int:
-    x = ((x & 0x05) << 1) | ((x & 0x0A) >> 1) | (x & 0x10)
-    x = ((x & 0x03) << 2) | ((x & 0x0C) >> 2) | (x & 0x10)
-    x = ((x & 0x0F) << 1) | ((x & 0x10) >> 4)
-    return x
-
-class ReadError(Exception):
-     pass
-class BaudotMsbFirstBytesReader(baudot.handlers.core.BaudotReader):
-    def __init__(self, stream: BufferedIOBase):
-        self.stream = stream
-
-    def __next__(self):
-        next_byte = self.stream.read(1)
-        if not next_byte:
-            raise StopIteration()
-        try:
-            code = next_byte[0]
-            code = reverse_mask_5_bits(code)
-        except ValueError:
-            str_repr = next_byte.decode(errors='backslashreplace')
-            raise ReadError(f'Invalid hexadecimal byte: {str_repr}')
-        if not 0 <= code < 32:
-            raise ReadError(f'Code value {code} is not a valid 5-bit value')
-        # print(code)
-        return code
-
-# ITA2_SWITCH_CODE = 0x1F
-code = bytes([0x1F]) + split_country_code_baudot_chars_in_bytes(714)
-print(f"Baudot code with LSB on the right, split in bytes, in hex: {code.hex()}")
-with BytesIO(code) as country_code_bitstream:
-        reader = BaudotMsbFirstBytesReader(country_code_bitstream)
-        country_code_str = baudot.decode_to_str(reader, baudot.codecs.ITA2_STANDARD)
-print(country_code_str)
+print(decode_country_code_from_int(country_code))
+print(decode_country_code_from_hex_str('B280'))
+print(decode_country_code_from_hex_str('B28'))
