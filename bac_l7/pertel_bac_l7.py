5b1df25c6f60c9eeb25c370b666ba1eaf6762ab4
Wesley <machado.wesley1998@gmail.com>
2025-03-31 20:54:09 +0200

    Fixed major bug when using close_transaction bool
    This caused transactions to be closed when sending normal DSRC L7
    commands and vice-versa (boolean was basically inverted).

--- bac_l7/pertel_bac_l7.py
+++ bac_l7/pertel_bac_l7.py
@@ -161,9 +161,9 @@ class PertelBacL7(bac_l2_host2beacon.BacHost):
 
     async def _pertel_send_dsrc_l7_command_with_close_transaction_option(self, t_apdu_containing_request:bytes, close_transaction:bool) -> bytes:
         if close_transaction:
-            return await self._pertel_send_dsrc_l7_command(t_apdu_containing_request)
-        else:
             return await self._pertel_send_dsrc_l7_command_and_close_transaction(t_apdu_containing_request)
+        else:
+            return await self._pertel_send_dsrc_l7_command(t_apdu_containing_request)
 
     async def _pertel_send_dsrc_l7_command(self, t_apdu_containing_request:bytes) -> bytes:
         """
