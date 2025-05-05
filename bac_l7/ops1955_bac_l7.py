184f561e34b978fe0d5171e855d906aff82e55b4
Wesley <machado.wesley1998@gmail.com>
2025-05-05 09:10:25 +0200

    Refactored command SET_DSRC_CONFIG (0x61)

--- bac_l7/ops1955_bac_l7.py
+++ bac_l7/ops1955_bac_l7.py
@@ -16,11 +16,60 @@ class Ops1955BacL7(pertel_bac_l7.PertelBacL7):
         await self._pertel_monitor_beacon()
 
         await self._kapsch_cd_read_dsrc_config()
-        await self._kapsch_cd_set_dsrc_config()
+        await self._kapsch_cd_set_dsrc_config_from_settings()
         # We now set its mode to transparent and stop serial communication!
         await self._pertel_set_beacon_mode(0x01)
 
-    async def _kapsch_cd_set_dsrc_config(self) -> bytes:
+    async def _kapsch_cd_set_dsrc_config(self,
+        msg_data_struct_version: int,
+        dsrc_channel: int,
+        bst_repetition_time_ms: int,
+        beacon_id_behavior: int,
+        end_transceiver_behavior:int
+        ) -> bytes:
+        """
+        Kapsch-specific.
+        CD_SET_DSRC_CONFIG
+        Message ID 0x61
+
+        msg_data_struct_version (int)
+            0x01: Version of the data structure
+
+        dsrc_channel (int)
+            0x01: 5,7975 GHz
+            0x02: 5,8025 GHz
+            0x03: 5,8075 GHz unused
+            0x04: 5,8125 GHz unused
+
+        bst_repetition_time_ms (int)
+            3 – 255
+            Is the minimum time in ms between
+            sequentially transmitted BSTs.
+        
+        beacon_id_behavior (int)
+            0x00: The individual part of the
+            beacon ID is managed by the device
+            and incremented after no reply.
+            0x01: The individual part of the
+            beacon ID is managed by the host.
+            0x02: The beacon ID is managed by
+            the device and is incremented after
+            each transaction.
+
+        """
+        msg_id = 0x61 # 97
+
+        message_content = bytes([msg_id, msg_data_struct_version, dsrc_channel, bst_repetition_time_ms, beacon_id_behavior, end_transceiver_behavior])
+        response = await self.send_command(message_content=message_content)
+
+        error_code_int = response[1]
+        if error_code_int == 0x03:
+            raise Exception("Transceiver not OK")
+        if error_code_int == 0x0B:
+            raise Exception("Command refused because the parameters are wrong")
+        return response
+
+    async def _kapsch_cd_set_dsrc_config_from_settings(self) -> bytes:
         """
         Kapsch-specific.
         CD_SET_DSRC_CONFIG
@@ -46,14 +95,7 @@ class Ops1955BacL7(pertel_bac_l7.PertelBacL7):
         elif bac_l2_config['release_command_config'] == 'ACn':
             end_transceiver_behavior = 0x01
 
-        message_content = bytes([msg_id, msg_data_struct_version, dsrc_channel, bst_repetition_time_ms, beacon_id_behavior, end_transceiver_behavior])
-        response = await self.send_command(message_content=message_content)
-
-        error_code_int = response[1]
-        if error_code_int == 0x03:
-            raise Exception("Transceiver not OK")
-        if error_code_int == 0x0B:
-            raise Exception("Command refused because the parameters are wrong")
+        response = await self._kapsch_cd_set_dsrc_config(msg_data_struct_version, dsrc_channel, bst_repetition_time_ms, beacon_id_behavior, end_transceiver_behavior)
         return response
 
     async def _kapsch_cd_read_dsrc_config(self) -> bytes:
