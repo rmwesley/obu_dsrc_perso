b0e8aa5abd9f53d9586cb6acff8370e1824ff3e7
Wesley <machado.wesley1998@gmail.com>
2025-05-12 14:36:58 +0200

    Implemented simple Beacon Client API
    For now this API only provides us an endpoint to sync transaction data
    from the Beacon Client to the Proxy Server.
    Soon we will implement a Beacon Proxy Server app.

---
+++ beacon_client_api_main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
+
+from routers import dsrc_transaction_data
+
+import logging
+root_logger = logging.getLogger()
+root_logger.setLevel(logging.DEBUG)
+
+root_app = FastAPI(title="Beacon Client API")
+
+@root_app.get('/', include_in_schema=False)
+async def get_index():
+    return FileResponse('beacon_client_front/static/index.html')
+
+@root_app.get('/home.svg', include_in_schema=False)
+async def favicon():
+    return FileResponse('beacon_client_front/static/home.svg')
+
+root_app.include_router(dsrc_transaction_data.router)
+
+# Serve the static HTML files for each subapp
+root_app.mount("/", StaticFiles(directory="beacon_client_front/static/"), name="static")
