892e7900642626a6f387afd89f3ab40e5355f6ca
Wesley <machado.wesley1998@gmail.com>
2025-07-15 20:10:27 +0200

    transactions-data webapp URI path is now /hmi

--- transaction_data_app.py
+++ transaction_data_app.py
@@ -11,4 +11,4 @@ transaction_data_app = FastAPI(title="Transaction Data API")
 transaction_data_app.include_router(dsrc_transaction_data.router)
 
 # Serve the static HTML files for this app
-transaction_data_app.mount("/transaction-data", StaticFiles(directory="fronts/web/transaction_data_web_front", html=True), name="tolling_testing_web_front_files")
+transaction_data_app.mount("/hmi", StaticFiles(directory="fronts/web/transaction_data_web_front", html=True), name="tolling_testing_web_front_files")
