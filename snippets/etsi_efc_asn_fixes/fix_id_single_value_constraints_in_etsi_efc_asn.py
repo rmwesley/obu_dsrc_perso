87e53cb021dffd57f50b74f659cb472a18a86164
Wesley <machado.wesley1998@gmail.com>
2025-07-18 20:46:39 +0200

    Improved ETSI EFC ASN.1 specs fix code execution

--- snippets/etsi_efc_asn_fixes/fix_id_single_value_constraints_in_etsi_efc_asn.py
+++ snippets/etsi_efc_asn_fixes/fix_id_single_value_constraints_in_etsi_efc_asn.py
@@ -1,18 +1,20 @@
-asn_filename = 'EFC-ACSE_V3.0.0.asn'
-asn_filename = 'EFC-ASO_V3.0.0.asn'
+def fix_etsi_asn_file(asn_filename):
+    with open(f'ASN/temp/ETSI ES 200 674-3-1 V3.0.0 (2010-06)/{asn_filename}', 'r') as asn_file:
+        asn_str = asn_file.read()
 
-with open(f'ASN/temp/ETSI ES 200 674-3-1 V3.0.0 (2010-06)/{asn_filename}', 'r') as asn_file:
-    asn_str = asn_file.read()
+    new_lines = []
+    for line in asn_str.split('\n'):
+        if "INTEGER ::= " in line:
+            fixed_line = line.replace(" INTEGER ::= ", " ::=")
+        elif "::= '" in line:
+            fixed_line = line.replace(" ::= '", " ('").replace("'H", "'H)")
+        else:
+            fixed_line = line
+        new_lines.append(fixed_line)
 
-new_lines = []
-for line in asn_str.split('\n'):
-    if "INTEGER ::= " in line:
-        fixed_line = line.replace(" INTEGER ::= ", " ::=")
-    elif "::= '" in line:
-        fixed_line = line.replace(" ::= '", " ('").replace("'H", "'H)")
-    else:
-        fixed_line = line
-    new_lines.append(fixed_line)
+    with open(f'ASN/temp/ETSI ES 200 674-3-1 V3.0.0 (2010-06)_fixed/{asn_filename}', 'w') as asn_out_file:
+        asn_out_file.write('\n'.join(new_lines))
 
-with open(f'ASN/temp/ETSI ES 200 674-3-1 V3.0.0 (2010-06)_fixed/{asn_filename}', 'w') as asn_out_file:
-    asn_out_file.write('\n'.join(new_lines))
+if __name__ == '__main__':
+    fix_etsi_asn_file('EFC-ACSE_V3.0.0.asn')
+    fix_etsi_asn_file('EFC-ASO_V3.0.0.asn')
