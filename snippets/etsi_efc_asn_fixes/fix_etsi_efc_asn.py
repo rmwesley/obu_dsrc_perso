18dea623d9d741f80326e6ef470f212248f61f29
Wesley <machado.wesley1998@gmail.com>
2025-07-18 21:42:49 +0200

    Fixed issue of SEQUENCEs with fixed value contents

--- snippets/etsi_efc_asn_fixes/fix_etsi_efc_asn.py
+++ snippets/etsi_efc_asn_fixes/fix_etsi_efc_asn.py
@@ -1,8 +1,8 @@
-def fix_etsi_asn_txt_svc(asn_str:str):
+def fix_etsi_asn_txt_sequence_fixed_field_values(asn_str:str):
     new_lines = []
     for line in asn_str.split('\n'):
         if "::= '" in line:
-            fixed_line = line.replace(" ::= '", " ('").replace("'H", "'H)")
+            fixed_line = line.replace(" ::= ", " ")
         else:
             fixed_line = line
         new_lines.append(fixed_line)
@@ -12,7 +12,7 @@ def fix_etsi_asn_txt_svc(asn_str:str):
 def fix_etsi_asn_file(asn_filename):
     with open(f'ASN/temp/ETSI ES 200 674-3-1 V3.0.0 (2010-06)/{asn_filename}', 'r') as asn_file:
         asn_str = asn_file.read()
-    new_asn_str = fix_etsi_asn_txt_svc(asn_str)
+    new_asn_str = fix_etsi_asn_txt_sequence_fixed_field_values(asn_str)
     with open(f'ASN/temp/ETSI ES 200 674-3-1 V3.0.0 (2010-06)_fixed/{asn_filename}', 'w') as asn_out_file:
         asn_out_file.write(new_asn_str)
 
