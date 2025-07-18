505e7bebc55d28c7582fe8d45d269a96d7a57663
Wesley <machado.wesley1998@gmail.com>
2025-07-18 20:34:18 +0200

    Improved fixes to ETSI EFC ASN.1 specs

--- snippets/etsi_efc_asn_fixes/fix_id_single_value_constraints_in_etsi_efc_asn.py
+++ snippets/etsi_efc_asn_fixes/fix_id_single_value_constraints_in_etsi_efc_asn.py
@@ -1,16 +1,18 @@
 asn_filename = 'EFC-ACSE_V3.0.0.asn'
-# asn_filename = 'EFC-ASO_V3.0.0.asn'
+asn_filename = 'EFC-ASO_V3.0.0.asn'
 
 with open(f'ASN/temp/ETSI ES 200 674-3-1 V3.0.0 (2010-06)/{asn_filename}', 'r') as asn_file:
     asn_str = asn_file.read()
 
 new_lines = []
 for line in asn_str.split('\n'):
-    if line.lstrip().startswith('id') and '::=' in line:
-        fixed_line = line.replace("::= '", "('").replace("'H", "'H)")
-        new_lines.append(fixed_line)
+    if "INTEGER ::= " in line:
+        fixed_line = line.replace(" INTEGER ::= ", " INTEGER (") + ")"
+    elif "::= '" in line:
+        fixed_line = line.replace(" ::= '", " ('").replace("'H", "'H)")
     else:
-        new_lines.append(line)
+        fixed_line = line
+    new_lines.append(fixed_line)
 
 with open(f'ASN/temp/ETSI ES 200 674-3-1 V3.0.0 (2010-06)_fixed/{asn_filename}', 'w') as asn_out_file:
     asn_out_file.write('\n'.join(new_lines))
