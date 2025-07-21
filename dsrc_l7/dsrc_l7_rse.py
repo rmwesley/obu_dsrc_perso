04f97d01336e1b411a845bb64dd364e11fa67667
Wesley <machado.wesley1998@gmail.com>
2025-07-21 16:05:07 +0200

    Bugfix: Transaction init data was being erased

--- dsrc_l7/dsrc_l7_rse.py
+++ dsrc_l7/dsrc_l7_rse.py
@@ -356,7 +356,7 @@ def create_transaction_data_file_from_init_phase_data(initialization_request_jva
     # Actual data at the bottom!
     transaction_data['data'] = {'initialization_phase': initialization_data}
     # Create an empty list for future data exchanges (ACTION/GET/SET requests...)
-    transaction_data['data'] = {'transaction_phase':  []}
+    transaction_data['data'] |= {'transaction_phase':  []}
 
     current_transaction_start_date = datetime.now()
     current_transaction_datetime_prefix = current_transaction_start_date.strftime("%Y%m%dT%H%M%S")
