f14e09f4101b2381547b9662861a2ac68a864107
Wesley <machado.wesley1998@gmail.com>
2025-07-30 16:32:25 +0200

    Renamed some transaction _data vars to _info
    position_info is a part of the transaction_info data.

--- routers/dsrc_transaction_data.py
+++ routers/dsrc_transaction_data.py
@@ -34,12 +34,7 @@ def compute_gnss_status_position_fixes_interpolation(gnss_fix_1, t1, gnss_fix_2,
     }
     return gnss_fix_interpolation
 
-# def compute_gnss_status_position_fixes_interpolation_from_obu_times(gnss_fix_1, gnss_fix_2, timestampstamp):
-#     t1 = previous_gnss_fix['lastGnssFixTime']
-#     t2 = current_gnss_fix['lastGnssFixTime']
-#     return compute_gnss_status_position_fixes_interpolation(gnss_fix_1, t1, gnss_fix_2, t2, timestampstamp)
-
-def add_gnss_fix_interpolation_to_transaction_data(transaction_with_fix_1, transaction_with_fix_2, transaction_data):
+def add_gnss_fix_interpolation_to_transaction_info(transaction_with_fix_1, transaction_with_fix_2, transaction_data):
     t1 = datetime.datetime.fromisoformat(transaction_with_fix_1['creation_time']).timestamp()
     t2 = datetime.datetime.fromisoformat(transaction_with_fix_2['creation_time']).timestamp()
     timestamp = datetime.datetime.fromisoformat(transaction_data['creation_time']).timestamp()
@@ -49,16 +44,16 @@ def add_gnss_fix_interpolation_to_transaction_data(transaction_with_fix_1, trans
         transaction_with_fix_2['position_info'], t2,
         timestamp)
 
-def interpolate_missing_gnss_fixes_in_transactions_list(transactions_data_list:list[dict]):
+def interpolate_missing_gnss_fixes_in_transactions_list(transactions_info_list:list[dict]):
     # actual_gnss_fix_indexes = []
     previous_index = 0
-    for curr_index, curr_transaction_data in enumerate(transactions_data_list):
-        if 'position_info' in curr_transaction_data and curr_transaction_data['position_info']:
+    for curr_index, curr_transaction_info in enumerate(transactions_info_list):
+        if 'position_info' in curr_transaction_info and curr_transaction_info['position_info']:
             for interpolation_index in range(previous_index+1, curr_index):
-                add_gnss_fix_interpolation_to_transaction_data(
-                    transactions_data_list[previous_index],
-                    transactions_data_list[curr_index],
-                    transactions_data_list[interpolation_index],
+                add_gnss_fix_interpolation_to_transaction_info(
+                    transactions_info_list[previous_index],
+                    transactions_info_list[curr_index],
+                    transactions_info_list[interpolation_index],
                 )
             previous_index = curr_index
 
@@ -75,14 +70,14 @@ def compute_gnss_status_position_fix_diff(previous_gnss_fix, current_gnss_fix):
     }
     return gnss_fix_delta
 
-def add_gnss_fix_delta_to_transaction_data(previous_gnss_fix, current_transaction_data):
+def add_gnss_fix_delta_to_transaction_info(previous_gnss_fix, current_transaction_data):
     current_transaction_data['_gnss_fix_delta'] = compute_gnss_status_position_fix_diff(previous_gnss_fix, current_transaction_data['position_info'])
 
 def add_gnss_fix_deltas_to_transaction_list(transaction_data_list: list[dict]):
     last_position = {}
     for transaction_data in transaction_data_list:
         if 'position_info' in transaction_data and transaction_data['position_info']:
-            add_gnss_fix_delta_to_transaction_data(last_position, transaction_data)
+            add_gnss_fix_delta_to_transaction_info(last_position, transaction_data)
             last_position = transaction_data['position_info']
     return transaction_data_list
 
@@ -98,14 +93,14 @@ async def get_transaction_info_for_obu_id(
     The OBU ID must be in hexadecimal format.
     """
     equOBUId = equOBUId.lower()
-    transactions_data_generator = transactions_data_db_operations.get_transactions_info_for_equ_obu_id(equOBUId, skip, limit, since_dt, until_dt)
+    transactions_info_generator = transactions_data_db_operations.get_transactions_info_for_equ_obu_id(equOBUId, skip, limit, since_dt, until_dt)
 
-    transaction_data_list = list(transactions_data_generator)
-    if interpolate_missing_gnss_fixes:
-        interpolate_missing_gnss_fixes_in_transactions_list(transaction_data_list)
+    transaction_info_list = list(transactions_info_generator)
+    if interpolate_missing_gnss_fixes == True:
+        interpolate_missing_gnss_fixes_in_transactions_list(transaction_info_list)
     if add_gnss_fix_deltas:
-        add_gnss_fix_deltas_to_transaction_list(transaction_data_list)
-    return transaction_data_list
+        add_gnss_fix_deltas_to_transaction_list(transaction_info_list)
+    return transaction_info_list
 
 @router.get('/transactions/{transaction_id}')
 async def get_transaction_data(transaction_id:str):
