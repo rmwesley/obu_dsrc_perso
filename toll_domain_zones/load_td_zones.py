859613c4f1457f5087baf34d1f2401eb4ae73363
Wesley <machado.wesley1998@gmail.com>
2025-07-31 11:44:55 +0200

    Script to convert shapefiles to geoJSON format

---
+++ toll_domain_zones/load_td_zones.py
@@ -0,0 +1,30 @@
+import json
+import pathlib
+
+import shapefile
+
+with pathlib.Path("settings/toll_domain_config.json").open('r') as json_file:
+    toll_domain_config = json.load(json_file)
+    shapefiles_filename_stem = toll_domain_config['td_zones_gis']['shapefiles_filename_stem']
+    td_zones_shp_path = pathlib.Path(shapefiles_filename_stem)
+
+shp_reader = shapefile.Reader(td_zones_shp_path)
+
+geo_json_feature_list = []
+for shape_record in shp_reader.shapeRecords():
+    geo_json_geometry = shape_record.shape.__geo_interface__
+
+    properties = shape_record.record.as_dict()
+    geo_json_feature = {
+        "type": "Feature",
+        "geometry": geo_json_geometry,
+        "properties": properties,
+    }
+    geo_json_feature_list.append(geo_json_feature)
+
+geo_json_data = {
+    "type": "FeatureCollection",
+    "features": geo_json_feature_list
+}
+with pathlib.Path("local_file_storage/td_zones_files/td_zones_geojson.json").open("w") as geo_json_file:
+    json.dump(geo_json_data, geo_json_file, indent=2)
