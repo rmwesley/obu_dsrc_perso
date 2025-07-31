46d4538171a212184be67fec84309009e96b720b
Wesley <machado.wesley1998@gmail.com>
2025-07-31 13:56:06 +0200

    SetMMI on transaction loop (1 beep every 5 secs)

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ async def simple_bcm_transactions():
 async def toll_domains_transaction_loop(td_list:list[str] = ['TIS', 'EasyGo', 'DE', 'CH', 'BE']):
     await dsrc_l7_rse.init_bcm_and_set_transparent_mode()
 
-    await dsrc_l7_transactions.loop_transactions_on_toll_domains(beep_state=False, td_list=td_list, sleep_time=5.0)
+    await dsrc_l7_transactions.loop_transactions_on_toll_domains(beep_state=True, td_list=td_list, sleep_time=5.0)
 
 # Main execution
 if __name__ == "__main__":
