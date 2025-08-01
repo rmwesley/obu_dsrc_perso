5bd17107ea9b5bfdf0f5bcf398ae4940ab8fcc8e
Wesley <machado.wesley1998@gmail.com>
2025-08-02 01:23:35 +0200

    TollDomainName detection from GPS position

---
+++ toll_domain_zones/td_geometry_operations.py
@@ -0,0 +1,25 @@
+import json
+import pathlib
+
+import shapefile
+import shapely
+
+with pathlib.Path("settings/toll_domain_config.json").open('r') as json_file:
+    toll_domain_config = json.load(json_file)
+    shapefiles_filename_stem = toll_domain_config['td_zones_gis']['shapefiles_filename_stem']
+    td_zones_shp_path = pathlib.Path(shapefiles_filename_stem)
+
+def get_td_name_from_gps_coords(latitude:float, longitude:float):
+    gps_point = shapely.Point(longitude, latitude)
+    # print(gps_point)
+
+    shapefile_reader = shapefile.Reader(td_zones_shp_path)
+    for shape_record in shapefile_reader:
+        zone_polygon = shapely.Polygon(shape_record.shape.points)
+        if zone_polygon.contains(gps_point):
+            print(shape_record.record.as_dict())
+            return shape_record.record['TollDomain']
+
+if __name__ == '__main__':
+    td_name = get_td_name_from_gps_coords(45.7593685, 4.8557787)
+    print(td_name)
