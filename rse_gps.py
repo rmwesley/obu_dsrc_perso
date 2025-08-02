e73990d86ccd4e22308be0b402752588521a0a60
Wesley <machado.wesley1998@gmail.com>
2025-08-02 02:32:52 +0200

    RSE GPS: TD set from GPS WebSocket endpoint!!!

--- rse_gps.py
+++ rse_gps.py
@@ -2,15 +2,22 @@ from fastapi import FastAPI, WebSocket
 from fastapi.responses import HTMLResponse
 from starlette.websockets import WebSocketDisconnect
 
+from dsrc_security.dsrc_mk_by_device_and_td_loader import set_toll_domain
+from toll_domain_zones.td_geometry_operations import get_td_name_from_gps_coords
+
 rse_gps_app = FastAPI()
 
 @rse_gps_app.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket):
+async def gps_websocket_endpoint(websocket: WebSocket):
     print('GPS WS: Connected!!')
     await websocket.accept()
     try:
         while True:
             data = await websocket.receive_json()
             print(data)
+            gps_coords = data['coords']
+            td_name = get_td_name_from_gps_coords(gps_coords['latitude'], gps_coords['longitude'])
+            set_toll_domain(td_name)
+            print(f"Set Toll Domain to: {td_name}")
     except WebSocketDisconnect:
         print('GPS WS: Disconnected!!')
