cb7f86cc00179bd31c1f27636040eefcedff6d0d
Wesley <machado.wesley1998@gmail.com>
2025-08-04 19:02:15 +0200

    Main FastAPI app: Fixed WatchFiles logging issue

--- main_app.py
+++ main_app.py
@@ -11,15 +11,24 @@ from rse_gps_sync_default_td_update_app import rse_gps_td_app
 
 import logging
 root_logger = logging.getLogger()
-root_logger.setLevel(logging.DEBUG)
+root_logger.setLevel(logging.INFO)
+
+for name in root_logger.manager.loggerDict.keys():
+    if "watchfiles.main" in name:
+        # print(name)
+        logging.getLogger(name).disabled = True
+        logging.getLogger(name).propagate = False
+        pass
 
 from datetime import datetime
 log_file_date_prefix = datetime.now().strftime('%Y%m%d')
 # SETTING UP LOGGER FILE HANDLER
 file_handler = logging.FileHandler(f'logs/api_logs/{log_file_date_prefix}_api_logs.log')
 file_formatter = logging.Formatter("%(asctime)s - %(levelname)-8s - %(threadName)s - %(message)s")
+# file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)-8s - %(threadName)s - %(message)s")
 file_handler.setFormatter(file_formatter)
 root_logger.addHandler(file_handler)
+
 
 app = FastAPI(title="Main FastAPI app")
 
