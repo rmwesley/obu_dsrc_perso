644165b981cb4f243d531555337e5d1ce2c400b8
Wesley <machado.wesley1998@gmail.com>
2025-08-05 13:56:45 +0200

    Renamed default_td.txt file

--- dsrc_security/dsrc_default_td_value_handler.py
+++ dsrc_security/dsrc_default_td_value_handler.py
@@ -5,7 +5,7 @@ import logging
 td_config_logger = logging.getLogger(__name__)
 
 def get_default_toll_domain_name():
-    with open(f'settings/default_toll_domain_name.txt') as txt_file:
+    with open(f'settings/default_td.txt') as txt_file:
         return txt_file.read()
 
 def update_default_toll_domain_name(new_default_td_name:str):
@@ -15,6 +15,6 @@ def update_default_toll_domain_name(new_default_td_name:str):
         td_config_logger.debug(f"Default Toll Domain is already ({new_default_td_name}).")
         return
 
-    with open(f'settings/default_toll_domain_name.txt', 'w') as txt_file:
+    with open(f'settings/default_td.txt', 'w') as txt_file:
         txt_file.write(new_default_td_name)
     td_config_logger.info(f"Config updated, default Toll Domain set to ({new_default_td_name}).")
