cdaf2422fa01556c8d988aea8280c1bd4cd3470d
Wesley <machado.wesley1998@gmail.com>
2025-08-07 00:36:45 +0200

    Snippet: Get valid OBU models (with MKs) by TD

--- snippets/personalization/valid_obus_config.py
+++ snippets/personalization/valid_obus_config.py
@@ -15,4 +15,12 @@ def generate_valid_obu_models_by_td_config():
     with open('local_file_storage/valid_obu_models_by_td.json', 'w') as json_file:
         json.dump(valid_obu_models_by_td, json_file, indent=2)
 
-generate_valid_obu_models_by_td_config()
+def generate_valid_obu_models_from_efc_contracts_config():
+    valid_obu_models_by_td = {}
+    for toll_domain_name, mk_by_obu_contract_ref in dsrc_mk_by_device_and_td_loader.master_keys_by_toll_domain.items():
+        valid_obu_models_by_td[toll_domain_name] = list(map(lambda obu_contract_ref: obu_contract_ref[12:20], mk_by_obu_contract_ref.keys()))
+
+    with open('local_file_storage/valid_obu_models_by_td.json', 'w') as json_file:
+        json.dump(valid_obu_models_by_td, json_file, indent=2)
+
+generate_valid_obu_models_from_efc_contracts_config()
