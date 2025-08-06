23b5bf4719e2b90026a7610e88063ae74a500110
Wesley <machado.wesley1998@gmail.com>
2025-08-07 00:58:16 +0200

    OBU perso: Endpoint to validate+create perso tasks

--- routers/obu_personalization.py
+++ routers/obu_personalization.py
@@ -1,8 +1,14 @@
 from fastapi import APIRouter, HTTPException
+from pydantic import BaseModel, model_validator, field_validator
+from typing import Dict, List
 from enum import Enum
 
 import json
+import uuid
 import pathlib
+
+from pycrate.pycrate_asn1c.err import ASN1ObjErr
+from ASN.compiled_DSRC_instances import AXXESv1_2
 
 router = APIRouter(tags=['OBU Personalization routes'])
 
@@ -19,6 +25,107 @@ def get_valid_obu_contracts(td_name:str|None = None):
         return valid_obu_models_by_td[td_name]
     else:
         raise HTTPException(400, f'Invalid Toll Domain Name: ({td_name})')
+
+with pathlib.Path('settings/toll_domain_config.json').open('r') as json_file:
+    td_conf_by_td_name = json.load(json_file)['td_conf_by_td_name']
+
+class DsrcAttributesDict(BaseModel):
+    @model_validator(mode='after')
+    def check_attribute_ids(cls, dsrc_attributes_dict):
+        for attribute_id in dsrc_attributes_dict:
+            if attribute_id > 127 or attribute_id < 0:
+                raise AssertionError(f'Invalid Attribute Id value: {attribute_id}')
+        return dsrc_attributes_dict
+
+class TollDomainPersoData(BaseModel):
+    tollDomain: str
+    dsrcAttributesDict: dict
+
+    @field_validator('tollDomain',mode='after')
+    def check_toll_domain_name(cls, toll_domain_name):
+        if toll_domain_name not in valid_obu_models_by_td or toll_domain_name not in td_conf_by_td_name:
+            raise AssertionError(f'Invalid Toll Domain: ({toll_domain_name})')
+        return toll_domain_name
+
+class InvalidObuModelContractRef(Exception):
+    pass
+
+class PersoTaskData(BaseModel):
+    obuModelRef: str
+    dsrcMemoryData: List[TollDomainPersoData]
+
+    @model_validator(mode='after')
+    def check_obu_model_contracts_for_each_td(cls, values):
+        for td_perso_data in values.dsrcMemoryData:
+            toll_domain_name = td_perso_data.tollDomain
+            valid_obus = valid_obu_models_by_td[toll_domain_name]
+
+            if values.obuModelRef not in valid_obus:
+                raise InvalidObuModelContractRef(f'The OBU model contract reference (0x{values.obuModelRef}) is not supported by the Toll Domain ({toll_domain_name})!!')
+        return values
+
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                {
+                    "obuModelRef": '00075113',
+                    "dsrcMemoryData": [
+                        {
+                            'tollDomain': 'EasyGo',
+                            'dsrcAttributesDict': {0: '20B2803100066F'}
+                        },
+                        {
+                            'tollDomain': 'CH',
+                            'dsrcAttributesDict': {0: '20B2803100066F'}
+                        }
+                    ]
+                }
+            ]
+        }
+    }
+
+def validate_efc_toll_domain_dsrc_data(tollDomain: str, dsrcAttributesDict:DsrcAttributesDict):
+    for attribute_id, attribute_value in dsrcAttributesDict.items():
+        try:
+            # Validate attribute value against EfcContainer!
+            attributeValueUper = bytes.fromhex(attribute_value)
+            attribute_value = AXXESv1_2.EfcDsrcGeneric.EfcContainer.from_uper(attributeValueUper)
+        except ASN1ObjErr:
+            raise Exception(f'Invalid Attribute value (0x{attribute_value})! It should be a UPER-encoded Hex string representing an ISO14906 EfcContainer ASN1 value!')
+
+def validate_ccc_toll_domain_dsrc_data(tollDomain: str, dsrcAttributesDict:DsrcAttributesDict):
+    for attribute_id, attribute_value in dsrcAttributesDict.items():
+        try:
+            # Validate attribute value against CccContainer!
+            attributeValueUper = bytes.fromhex(attribute_value)
+            attribute_value = AXXESv1_2.EfcCcc.CccContainer.from_uper(attributeValueUper)
+        except ASN1ObjErr:
+            raise Exception(f'Invalid Attribute value (0x{attribute_value})! It should be a UPER-encoded Hex string representing an ISO14906 EfcContainer ASN1 value!')
+
+def validate_dsrc_data(tollDomain: str, dsrcAttributesDict:DsrcAttributesDict):
+    if 1 in td_conf_by_td_name[tollDomain]['mandApplications']:
+        validate_efc_toll_domain_dsrc_data(tollDomain, dsrcAttributesDict)
+    if 20 in td_conf_by_td_name[tollDomain]['mandApplications']:
+        validate_ccc_toll_domain_dsrc_data(tollDomain, dsrcAttributesDict)
+
+@router.post('/persos/')
+def create_personalization_task(perso_task_data:PersoTaskData):
+    for tollDomainPersoData in perso_task_data.dsrcMemoryData:
+        tollDomain = tollDomainPersoData.tollDomain
+        dsrcAttributesDict = tollDomainPersoData.dsrcAttributesDict
+
+        validate_dsrc_data(tollDomain, dsrcAttributesDict)
+
+    perso_id = uuid.uuid4()
+    perso_data_filepath = perso_tasks_dirpath / f'{perso_id}.json'
+
+    with perso_data_filepath.open('w') as json_file:
+        json_file.write(perso_task_data.model_dump_json(indent=2))
+
+    return {
+        'message': 'Personalization Request received!',
+        'perso_id': perso_id
+    }
 
 class PersoRequestState(str, Enum):
     PENDING = 'pending'
