0dc47486b5f309ce54bb87d1dd95dba9d10fd765
Wesley <machado.wesley1998@gmail.com>
2025-08-06 19:58:16 +0200

    Pero app: Web App/Front placeholder

--- fastapi_apps/personalization_app.py
+++ fastapi_apps/personalization_app.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 from routers import obu_personalization
 
 personalization_app = FastAPI(title='OBU Personalization app')
 personalization_app.include_router(prefix='/data', router=obu_personalization.router)
+
+personalization_app.mount("/hmi", StaticFiles(directory="fronts/web/perso_app_web_front", html=True), name="perso_app_web_front_files")
