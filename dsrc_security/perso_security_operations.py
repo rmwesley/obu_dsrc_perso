d4b6ae3324dd120523916759651a9ffef99427fb
Wesley <machado.wesley1998@gmail.com>
2025-08-19 00:41:30 +0200

    TSP Sec: Decrypt with USET MKs of length 8, 16, 32

--- dsrc_security/perso_security_operations.py
+++ dsrc_security/perso_security_operations.py
@@ -1,4 +1,4 @@
-from Crypto.Cipher import DES3
+from Crypto.Cipher import DES, DES3
 
 import json
 import logging
@@ -13,6 +13,7 @@ with open('../security_info/tsp_obu_setup/perso_config_and_obu_models_v1.0.1.jso
 with open('../security_info/tsp_obu_setup/axxes_kapsch_tis_uset_master_keys.json', 'r') as json_file:
     axxes_kapsch_uset_master_keys = json.load(json_file)[perso_env]
 
+default_uset_key_type = 'Stock'
 TRP_4010_20B_MK_TYPES = typing.Literal['Factory', 'Stock', 'Exploit']
 def set_default_key_type(uset_key_type:TRP_4010_20B_MK_TYPES):
     global default_uset_key_type
@@ -49,13 +50,33 @@ def get_uset_derived_key_for_obu_model(obu_model:str, ac_cr_key_ref:int, uset_ke
 
     return get_uset_derived_key(uset_master_key=uset_mk_bytes, ac_cr_key_ref=ac_cr_key_ref)
 
-def decrypt_uset_derived_key(uset_master_key:bytes, ciphertext:bytes) -> bytes:
+def decrypt_uset_derived_key_8_bytes_mk(uset_master_key:bytes, ciphertext:bytes) -> bytes:
+    cipher = DES.new(key=uset_master_key[0:8], mode=DES3.MODE_ECB)
+    plaintext = cipher.decrypt(ciphertext[0:8])
+
+    return plaintext
+
+def decrypt_uset_derived_key_16_bytes_mk(uset_master_key:bytes, ciphertext:bytes) -> bytes:
+    cipher = DES3.new(key=uset_master_key[0:16], mode=DES3.MODE_ECB)
+    plaintext = cipher.decrypt(ciphertext[0:8])
+
+    return plaintext
+
+def decrypt_uset_derived_key_32_bytes_mk(uset_master_key:bytes, ciphertext:bytes) -> bytes:
     cipher1 = DES3.new(key=uset_master_key[0:16], mode=DES3.MODE_ECB)
     plaintext_part1 = cipher1.decrypt(ciphertext[0:8])
     cipher2 = DES3.new(key=uset_master_key[16:32], mode=DES3.MODE_ECB)
     plaintext_part2 = cipher2.decrypt(ciphertext[8:16])
 
     return plaintext_part1 + plaintext_part2
+
+def decrypt_uset_derived_key(uset_master_key:bytes, ciphertext:bytes) -> bytes:
+    if len(uset_master_key) == 8:
+        return decrypt_uset_derived_key_8_bytes_mk(uset_master_key, ciphertext)
+    if len(uset_master_key) == 16:
+        return decrypt_uset_derived_key_16_bytes_mk(uset_master_key, ciphertext)
+    if len(uset_master_key) == 32:
+        return decrypt_uset_derived_key_32_bytes_mk(uset_master_key, ciphertext)
 
 def decrypt_uset_derived_key_for_obu_model(obu_model:str, ciphertext:bytes, uset_key_type=default_uset_key_type) -> bytes:
     if not uset_key_type:
@@ -65,5 +86,3 @@ def decrypt_uset_derived_key_for_obu_model(obu_model:str, ciphertext:bytes, uset
     uset_mk_bytes = bytes.fromhex(uset_mk_hex)
 
     return decrypt_uset_derived_key(uset_master_key=uset_mk_bytes, ciphertext=ciphertext)
-
-set_default_key_type('Stock')
