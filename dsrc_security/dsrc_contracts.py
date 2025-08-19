bed79c811fa468bd95bc90319ad4df0d82828b7e
Wesley <machado.wesley1998@gmail.com>
2025-08-19 22:09:28 +0200

    Contracts: OBE config is at VST root, not each app

--- dsrc_security/dsrc_contracts.py
+++ dsrc_security/dsrc_contracts.py
@@ -17,15 +17,16 @@ def is_device_info_valid_in_current_td(efc_cm: str|int|bytes, manufacturer_id:st
 def get_eid_in_vst_with_valid_contract_in_current_td(vst_value: dict = None) -> int:
     """Get EID in VST with a valid EFC-CM for the current Toll Domain."""
     available_applications_list = vst_value['applications']
+
+    obe_config = vst_value['obeConfiguration']
+    equipment_class = obe_config['equipmentClass']
+    manufacturer_id = obe_config['manufacturerID']
+    dsrc_contracts_logger.info(f'OBU equipment ref: 0x{manufacturer_id:04X}{equipment_class:04X}')
+
     for application_data in available_applications_list:
         app_parameter_type, app_parameter_value = application_data['parameter']
         efc_cm_bytes = app_parameter_value[0:6]
         dsrc_contracts_logger.debug(f'EFC-CM: 0x{efc_cm_bytes.hex().upper()}')
-
-        obe_config = vst_value['obeConfiguration']
-        equipment_class = obe_config['equipmentClass']
-        manufacturer_id = obe_config['manufacturerID']
-
         
         if is_device_info_valid_in_current_td(efc_cm_bytes, manufacturer_id, equipment_class):
             return application_data['eid']
