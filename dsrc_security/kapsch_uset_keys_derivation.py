02cb127cbba200695e330abcd790ee0579ceff35
Wesley <machado.wesley1998@gmail.com>
2025-08-22 14:26:22 +0200

    DSRC Perso: Kapsch USET key module update

--- dsrc_security/kapsch_uset_keys_derivation.py
+++ dsrc_security/kapsch_uset_keys_derivation.py
@@ -1,13 +1,14 @@
 from Crypto.Cipher import DES3
 
-with open('../security_info/axxes_master_uset_key_hex_v1.0.txt', 'r') as txt_file:
-    axxes_master_uset_key_hex = txt_file.read()
-    axxes_master_uset_key_bytes = bytes.fromhex(axxes_master_uset_key_hex)
+# EXPLOIT
+with open('../security_info/axxes_kapsch_exploit_master_uset_key_hex_v1.0.txt', 'r') as txt_file:
+    axxes_kapsch_exploit_master_uset_key_hex = txt_file.read()
+    axxes_kapsch_exploit_master_uset_key_bytes = bytes.fromhex(axxes_kapsch_exploit_master_uset_key_hex)
 
-    axxes_uset_cipher_part1 = DES3.new(axxes_master_uset_key_bytes[0:5], DES3.MODE_ECB)
-    axxes_uset_cipher_part2 = DES3.new(axxes_master_uset_key_bytes[5:10], DES3.MODE_ECB)
-    del axxes_master_uset_key_bytes
-    del axxes_master_uset_key_hex
+    axxes_uset_cipher_part1 = DES3.new(axxes_kapsch_exploit_master_uset_key_bytes[0:5], DES3.MODE_ECB)
+    axxes_uset_cipher_part2 = DES3.new(axxes_kapsch_exploit_master_uset_key_bytes[5:10], DES3.MODE_ECB)
+    del axxes_kapsch_exploit_master_uset_key_bytes
+    del axxes_kapsch_exploit_master_uset_key_hex
 
 def get_kapsch_ac_cr_keyref_from_obu_id(eq_obu_id_bytes:bytes) -> int:
     # AC_CR-KeyRef is two bytes (0x00 + OBU ID's least significant byte) = OBU ID modulo 256
@@ -18,7 +19,7 @@ def get_kapsch_ac_cr_keyref_from_serial_number(serial_number:str) -> int:
     obu_id_bytes = int(obu_id_str, 10).to_bytes(4)
     return get_kapsch_ac_cr_keyref_from_obu_id(obu_id_bytes)
 
-def get_uset_derived_key_bytes(ac_cr_keyref:int) -> bytes:
+def get_uset_exploit_derived_key_bytes(ac_cr_keyref:int) -> bytes:
     # USET Key = 3DES(Master USET Key Part 1, AC_CR-KeyRef || AC_CR-KeyRef || AC_CR-KeyRef || AC_CRKeyRef) || 3DES(Master USET Key Part 2, AC_CR-KeyRef || AC_CR-KeyRef || AC_CR-KeyRef || AC_CRKeyRef)
     plaintext_bytes = ac_cr_keyref.to_bytes(4) * 4
 
@@ -28,10 +29,18 @@ def get_uset_derived_key_bytes(ac_cr_keyref:int) -> bytes:
     uset_key = ciphertext_bytes_part1 + ciphertext_bytes_part2
     return uset_key
 
-def get_uset_derived_key_bytes_from_obu_id(eq_obu_id_bytes:bytes) -> bytes:
-    ac_cr_keyref = get_kapsch_ac_cr_keyref_from_obu_id(eq_obu_id_bytes)
-    return get_uset_derived_key_bytes(ac_cr_keyref)
+def get_uset_exploit_derived_key_bytes_from_eack(eack_bytes) -> bytes:
+    # USET = EAcK || EAcK
+    return eack_bytes * 2
 
-def get_uset_derived_key_bytes_from_serial_number(serial_number:str) -> bytes:
+def get_uset_exploit_derived_key_bytes_from_obu_id(eq_obu_id_bytes:bytes) -> bytes:
+    ac_cr_keyref = get_kapsch_ac_cr_keyref_from_obu_id(eq_obu_id_bytes)
+    return get_uset_exploit_derived_key_bytes(ac_cr_keyref)
+
+def get_uset_exploit_derived_key_bytes_from_serial_number(serial_number:str) -> bytes:
     ac_cr_keyref = get_kapsch_ac_cr_keyref_from_serial_number(serial_number)
-    return get_uset_derived_key_bytes(ac_cr_keyref)
+    return get_uset_exploit_derived_key_bytes(ac_cr_keyref)
+
+# TRANSPORT
+
+# STOCK
