737293e95b42404651a7a244e0ba4e42ea240f43
Wesley <machado.wesley1998@gmail.com>
2025-08-29 16:41:05 +0200

    Perso router: POST /kapsch_dsrc_uset_perso_apply
    Directly apply Kapsch USET perso to OBU

--- routers/obu_dsrc_personalization.py
+++ routers/obu_dsrc_personalization.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel, model_validator, field_validator
 from typing import Dict, List, Literal
 from enum import Enum
@@ -225,3 +225,21 @@ def validate_obu_data_and_finish_perso(perso_id:str):
 #             attribute_value = AXXESv1_2.EfcDsrcGeneric.EfcContainer.from_uper(attributeValueUper)
 #         except ASN1ObjErr:
 #             raise Exception(f'Invalid Attribute value (0x{attribute_value})! It should be a UPER-encoded Hex string representing an ISO14906 EfcContainer ASN1 value!')
+@router.post('/kapsch_dsrc_uset_perso_apply/')
+async def apply_kapsch_personalization_data_to_obu(perso_task_data:PersoTaskData, obu_eq_ref:str = '00037404', uset_key_type:str = 'Stock'):
+    '''Request application of personalization to OBU through a DSRC beacon'''
+    try:
+        await dsrc_l7_rse.check_and_update_beacon_state()
+    except dsrc_l7_rse.NoBeaconInitialized as e:
+        raise HTTPException(409, detail=str(e))
+
+    dsrc_memory_data = {eid: el_dsrc_data.dsrcAttributesDict for eid, el_dsrc_data in perso_task_data.dsrcMemoryData.items()}
+    obu_id_hex = await dsrc_perso_l7.kapsch_trp_4010_20b_pl_perso(
+        dsrc_memory_data = dsrc_memory_data,
+        expected_obu_eq_ref = obu_eq_ref,
+        uset_key_type = uset_key_type
+        )
+
+    return {
+        'equOBUId_hex': obu_id_hex
+    }
