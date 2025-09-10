970cba54e4d1b48e042b700d4b9bcc1ac3e94204
Wesley <machado.wesley1998@gmail.com>
2025-09-10 19:39:51 +0200

    Fix: Return Kapsch USET bytes, not hex values

--- dsrc_security/kapsch_http_uset_key_obtention.py
+++ dsrc_security/kapsch_http_uset_key_obtention.py
@@ -15,30 +15,36 @@ def send_get_request_to_web_service(route:str):
 
 def compute_uset_derived_key_via_mk_name(uset_mk_name:str, ac_cr_key_ref:int) -> bytes:
     ac_cr_key_ref_hex = f'{ac_cr_key_ref:04X}'
-    route=f'/uset_masterkeys/{uset_mk_name}/derived_keys/{ac_cr_key_ref_hex}/'
-    return send_get_request_to_web_service(route)
+    route=f'uset_masterkeys/{uset_mk_name}/derived_keys/{ac_cr_key_ref_hex}'
+    response = send_get_request_to_web_service(route)
+    return bytes.fromhex(response.text)
 
 def get_decrypted_kapsch_uset_key_via_mk_name(uset_mk_name:str, uset_derived_key_hex:str):
-    route = f'/uset_masterkeys/{uset_mk_name}/decrypt_uset_key/{uset_derived_key_hex}'
-    return send_get_request_to_web_service(route)
+    route = f'uset_masterkeys/{uset_mk_name}/decrypt_uset_key/{uset_derived_key_hex}'
+    response = send_get_request_to_web_service(route)
+    return bytes.fromhex(response.text)
 
 def compute_kapsch_uset_access_credentials_via_mk_name(uset_mk_name:str, ac_cr_key_ref:int, rnd_obe:int) -> bytes:
     ac_cr_key_ref_hex = f'{ac_cr_key_ref:04X}'
     rnd_obe_hex = f'{rnd_obe:08X}'
-    route = f'/uset_masterkeys/{uset_mk_name}/derived_keys/{ac_cr_key_ref_hex}/ac_cr/{rnd_obe_hex}'
-    return requests.get(route)
+    route = f'uset_masterkeys/{uset_mk_name}/derived_keys/{ac_cr_key_ref_hex}/ac_cr/{rnd_obe_hex}'
+    response = send_get_request_to_web_service(route)
+    return bytes.fromhex(response.text)
 
 def get_uset_derived_key_for_obu_model(obu_model:str, uset_key_type, ac_cr_key_ref:int) -> bytes:
     ac_cr_key_ref_hex = f'{ac_cr_key_ref:04X}'
-    route = f'/obu_models/{obu_model}/uset_key_types/{uset_key_type}/derived_keys/{ac_cr_key_ref_hex}'
-    return send_get_request_to_web_service(route)
+    route = f'obu_models/{obu_model}/uset_key_types/{uset_key_type}/derived_keys/{ac_cr_key_ref_hex}'
+    response = send_get_request_to_web_service(route)
+    return bytes.fromhex(response.text)
 
 def get_decrypted_kapsch_uset_key_for_obu_model(obu_model_name:str, uset_key_type, uset_derived_key_hex:str):
-    route = f'/obu_models/{obu_model_name}/uset_key_types/{uset_key_type}/decrypt_uset_key/{uset_derived_key_hex}'
-    return send_get_request_to_web_service(route)
+    route = f'obu_models/{obu_model_name}/uset_key_types/{uset_key_type}/decrypt_uset_key/{uset_derived_key_hex}'
+    response = send_get_request_to_web_service(route)
+    return bytes.fromhex(response.text)
 
 def get_kapsch_uset_access_credentials_for_obu_model(obu_model:str, uset_key_type, ac_cr_key_ref:int, rnd_obe:int) -> bytes:
     ac_cr_key_ref_hex = f'{ac_cr_key_ref:04X}'
     rnd_obe_hex = f'{rnd_obe:08X}'
-    route = f'/obu_models/{obu_model}/uset_key_types/{uset_key_type}/derived_keys/{ac_cr_key_ref_hex}/ac_cr/{rnd_obe_hex}'
-    return send_get_request_to_web_service(route)
+    route = f'obu_models/{obu_model}/uset_key_types/{uset_key_type}/derived_keys/{ac_cr_key_ref_hex}/ac_cr/{rnd_obe_hex}'
+    response = send_get_request_to_web_service(route)
+    return bytes.fromhex(response.text)
